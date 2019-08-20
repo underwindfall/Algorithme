81852142510d34fa78746c6b9f9b36686a81abd1
Qifan Yang <yangqifan02@hotmail.com>
2019-08-20 13:53:33 +0200

    init project

---
+++ src/fbm.kt
@@ -0,0 +1,11 @@
+import java.math.BigInteger
+
+fun main(args: Array<String>) {
+    println(fibonacci().take(10).toList())
+}
+
+fun fibonacci(): Sequence<Int> {
+    // fibonacci terms
+    // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, ...
+    return generateSequence(Pair(0, 1), { Pair(it.second, it.first + it.second) }).map { it.first }
+}
