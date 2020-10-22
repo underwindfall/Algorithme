ee5fac216b601d26959d8bf64ada90eca54f4092
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-10-22 23:27:13 +0200

    移除元素

--- src/training/array05/TwoSum.java
+++ src/training/array05/TwoSum.java
@@ -1,9 +1,25 @@
 package training.array05;
 
+//https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+/**
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ * 
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ * 
+ * 说明:
+ * 
+ * 返回的下标值（index1 和 index2）不是从零开始的。 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。 
+ * 
+ * 示例:
+ * 
+ * 输入: numbers = [2, 7, 11, 15], target = 9 输出: [1,2] 解释: 2 与 7 之和等于目标数 9 。因此
+ * index1 = 1, index2 = 2 。
+ * 
+ */
 public class TwoSum {
     // 二分法
     // time: O(N*logN)
-    //espace: O(1)
+    // espace: O(1)
     static int[] twoSum(int[] numbers, int target) {
         for (int i = 0; i < numbers.length; i++) {
             int low = i + 1, high = numbers.length - 1;
@@ -21,23 +37,23 @@ public class TwoSum {
         return new int[] { -1, -1 };
     }
 
-    //双向指针法
-    //time: O(N/2) === O(N)
-    //espace: O(1)
+    // 双向指针法
+    // time: O(N/2) === O(N)
+    // espace: O(1)
     static int[] twoSum1(int[] numbers, int target) {
         int low = 0, high = numbers.length - 1;
         while (low < high) {
             int sum = numbers[low] + numbers[high];
             if (sum < target) {
-                low ++;
+                low++;
             } else if (sum > target) {
-                high --; 
+                high--;
             } else {
-                return new int[] {low + 1, high + 1};
+                return new int[] { low + 1, high + 1 };
             }
         }
-        return new int[] {-1, -1};
-      }
+        return new int[] { -1, -1 };
+    }
 
     public static void main(String[] args) {
         int[] numbers = new int[] { 2, 7, 11, 15 };
