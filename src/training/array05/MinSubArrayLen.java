e832fb8cf82c536de54f79da6f78e8b9d5f725b5
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-10-23 00:08:40 +0200

    长度最小的子数组

---
+++ src/training/array05/MinSubArrayLen.java
@@ -0,0 +1,43 @@
+package training.array05;
+
+//
+/**
+ * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的 连续
+ * 子数组，并返回其长度。如果不存在符合条件的子数组，返回 0。
+ * 
+ *  
+ * 
+ * 示例：
+ * 
+ * 输入：s = 7, nums = [2,3,1,2,4,3] 输出：2 解释：子数组 [4,3] 是该条件下的长度最小的子数组。  
+ * 
+ * 进阶：
+ * 
+ * 如果你已经完成了 O(n) 时间复杂度的解法, 请尝试 O(n log n) 时间复杂度的解法。
+ */
+public class MinSubArrayLen {
+    static int minSubArrayLen(int s, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= s) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minSubArrayLen(7, new int[] { 2, 3, 1, 2, 4, 3 }));
+    }
+}
