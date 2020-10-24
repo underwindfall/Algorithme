eaaffff1e4bdb5dc68ca82e03a6ab58b7a5855df
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-10-25 00:25:21 +0200

    环形链表

---
+++ src/training/linkedlist/LinkedListCycle.java
@@ -0,0 +1,52 @@
+package training.linkedlist;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedListCycle {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    // time: O(N)
+    // espace: O(N)
+    static boolean hasCycle(ListNode head) {
+        Set<ListNode> seen = new HashSet<>();
+        while (head != null) {
+            if (!seen.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    // time: O(N)
+    // espace: O(N)
+    static boolean hasCycle1(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
