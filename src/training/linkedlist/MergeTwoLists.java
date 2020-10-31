1b1c59f605c60666709c18cfa33cbec1fd5f2851
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-10-31 16:55:44 +0100

    合并两个有序链表

---
+++ src/training/linkedlist/MergeTwoLists.java
@@ -0,0 +1,73 @@
+package training.linkedlist;
+
+// https://leetcode-cn.com/problems/merge-two-sorted-lists/
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * 
+ *  
+ * 
+ * 示例：
+ * 
+ * 输入：1->2->4, 1->3->4 输出：1->1->2->3->4->4
+ * 
+ * 
+ */
+public class MergeTwoLists {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        // 迭代的方式
+        // time:
+        // espace:
+        public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+            ListNode prehead = new ListNode(0);
+            ListNode prev = prehead;
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    prev.next = l1;
+                    l1 = l1.next;
+                } else {
+                    prev.next = l2;
+                    l2 = l2.next;
+                }
+                prev = prev.next;
+            }
+
+            // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+            prev.next = l1 == null ? l2 : l1;
+            return prehead.next;
+        }
+
+        // 递归的方式
+        // list1[0]+merge(list1[1:],list2) list1[0]<list2[0]
+        // list2[0]+merge(list1,list2[1:]) otherwise
+        // time O(M+N)
+        // espace:O(M+N)
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            } else if (l2 == null) {
+                return l1;
+            } else if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+}
