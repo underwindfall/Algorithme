a6f3d484aa34cdcc56a5ae464ef458f9535e4c9e
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-11-06 02:09:09 +0100

    二叉树的最近公共祖先

--- src/training/binarytree/PopulateNextRightPointer2.java
+++ src/training/binarytree/PopulateNextRightPointer2.java
@@ -3,6 +3,19 @@ package training.binarytree;
 import java.util.HashMap;
 import java.util.Map;
 
+// https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii
+// 给定一个二叉树
+
+// struct Node {
+//   int val;
+//   Node *left;
+//   Node *right;
+//   Node *next;
+// }
+// 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+// 初始状态下，所有 next 指针都被设置为 NULL
+
 public class PopulateNextRightPointer2 {
 
     class Node {
@@ -22,7 +35,7 @@ public class PopulateNextRightPointer2 {
             val = _val;
             left = _left;
             right = _right;
-            next = _next; 
+            next = _next;
         }
     };
 
