54a0d741a55039cc26b6673f5c7ae0571489b004
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-11-10 19:09:15 +0100

    Leetcode 167 Two sum

---
+++ src/leetcode/datastructure/array/TwoSum167.java
@@ -0,0 +1,44 @@
+package leetcode.datastructure.array;
+
+public class TwoSum167 {
+    // time O(N*logN)
+    // espace O(1)
+    class Binary {
+        public int[] twoSum(int[] numbers, int target) {
+            for (int i = 0; i < numbers.length; i++) {
+                int start = i + 1;
+                int end = numbers.length - 1;
+                while (start <= end) {
+                    int mid = (end - start) / 2 + start;
+                    if (numbers[i] + numbers[mid] > target) {
+                        end = mid - 1;
+                    } else if (numbers[i] + numbers[mid] < target) {
+                        start = mid + 1;
+                    } else {
+                        return new int[] { i + 1, mid + 1 };
+                    }
+                }
+            }
+
+            return new int[] { -1, -1 };
+        }
+    }
+
+    // time O(N)
+    // espace O(1)
+    class DoubleIndex {
+        public int[] twoSum(int[] numbers, int target) {
+            int start = 0, end = numbers.length - 1;
+            while (start < end) {
+                if (numbers[start] + numbers[end] > target) {
+                    end--;
+                } else if (numbers[start] + numbers[end] < target) {
+                    start++;
+                } else {
+                    return new int[] { start + 1, end + 1 };
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+    }
+}
