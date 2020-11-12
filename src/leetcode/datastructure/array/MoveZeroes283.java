bccc6abe3ec1150abc7db6a15c14c9802db56018
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-11-12 15:50:42 +0100

    Leetcode 283 move zeroes

---
+++ src/leetcode/datastructure/array/MoveZeroes283.java
@@ -0,0 +1,33 @@
+package leetcode.datastructure.array;
+// https://leetcode-cn.com/problems/move-zeroes
+public class MoveZeroes283 {
+    // 快慢指针
+    // time O(N)
+    // espace O(1)
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                if (fast > slow) {
+                    nums[slow] = nums[fast];
+                    nums[fast] = 0;
+                }
+                slow++;
+            }
+        }
+    }
+/*     public void moveZeroes(int[] nums) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        for (int i = slow; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    } */
+
+
+}
