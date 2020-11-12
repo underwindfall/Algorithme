8cfa7a7df0767a9b4a8470b9b172f3875cd7852f
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-11-12 14:59:45 +0100

    Leetcode 557 reverse words III

---
+++ src/leetcode/datastructure/array/ReverseWordsInString557.java
@@ -0,0 +1,29 @@
+package leetcode.datastructure.array;
+
+public class ReverseWordsInString557 {
+    // 快慢指针
+    // fast 循环单词 当遇到空格的时候停下 让slow向前走一步
+    // slow 从头开始
+    // time O(N^2/2)
+    // espace O(1)
+    public String reverseWords(String s) {
+        int slow = 0;
+        char[] wordArray = s.toCharArray();
+        for (int fast = 0; fast < wordArray.length; fast++) {
+            if (wordArray[fast] != ' ' && (fast + 1 >= wordArray.length || wordArray[fast + 1] == ' ')) {
+                // 翻转操作
+                int wordStart = slow;
+                int wordEnd = fast;
+                while (wordStart < wordEnd) {
+                    char tmp = wordArray[wordStart];
+                    wordArray[wordStart] = wordArray[wordEnd];
+                    wordArray[wordEnd] = tmp;
+                    wordStart++;
+                    wordEnd--;
+                }
+                slow = fast + 2;
+            }
+        }
+        return new String(wordArray);
+    }
+}
