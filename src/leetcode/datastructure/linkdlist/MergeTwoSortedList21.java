807da839726b70a389c2e01240d554347ff3545b
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-11-13 14:05:39 +0100

    Leetcode 21 merge two list

---
+++ src/leetcode/datastructure/linkdlist/MergeTwoSortedList21.java
@@ -0,0 +1,103 @@
+package leetcode.datastructure.linkdlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeTwoSortedList21 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // time O (N+N) = O(N)
+    // espace O(N1+N2)= O(N)
+    class ListSolution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            }
+            if (l2 == null) {
+                return l1;
+            }
+            List<ListNode> nodes = new ArrayList<>();
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    nodes.add(l1);
+                    l1 = l1.next;
+                } else {
+                    nodes.add(l2);
+                    l2 = l2.next;
+                }
+            }
+
+            for (int i = 0; i < nodes.size() - 1; i++) {
+                nodes.get(i).next = nodes.get(i + 1);
+            }
+
+            if (l1 != null) {
+                nodes.get(nodes.size() - 1).next = l1;
+            } else {
+                nodes.get(nodes.size() - 1).next = l2;
+            }
+            return nodes.get(0);
+        }
+    }
+
+    /**
+     * 
+     * 
+     * time O(N1+N2) = O(N)
+     * 
+     * espace O(N1+N2) = O(N)
+     */
+    class Recursive {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            } else if (l2 == null) {
+                return l1;
+            } else if (l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
+        }
+    }
+
+    class Iterative {
+        // time O(N1+N2) = O(N)
+        // espace O(1)
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            ListNode prehead = new ListNode(0);
+            ListNode prev = prehead;
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    prev.next = l1;
+                    l1 = l1.next;
+                } else {
+                    prev.next = l2;
+                    l2 = l2.next;
+                }
+                prev = prev.next;
+            }
+
+            // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+            prev.next = l1 == null ? l2 : l1;
+            return prehead.next;
+        }
+    }
+}
