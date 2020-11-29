3862b113f279e1cff173d31261a83b509c5dc6e6
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-11-29 23:26:26 +0100

    Leetcode 565 Array Nesting

---
+++ src/leetcode/datastructure/array/ArrayNesting565.java
@@ -0,0 +1,73 @@
+package leetcode.datastructure.array;
+
+import javax.swing.text.html.FormView;
+
+// https://leetcode-cn.com/problems/array-nesting/description/
+public class ArrayNesting565 {
+
+    // time O(N)
+    // espace O(1)
+    class MaxValueArraySolution {
+        public int arrayNesting(int[] nums) {
+            int maxSize = 0;
+            for (int i = 0; i < nums.length; i++) {
+                int start = nums[i], count = 0;
+                while (nums[i] != Integer.MAX_VALUE) {
+                    int temp = start;
+                    start = nums[temp];
+                    count++;
+                    nums[temp] = Integer.MAX_VALUE;
+                }
+                maxSize = Math.max(maxSize, count);
+            }
+            return maxSize;
+        }
+    }
+
+    // time O(N)
+    // espace O(1)
+    class BooleanArraySolution {
+        public int arrayNesting(int[] nums) {
+            boolean[] visited = new boolean[nums.length];
+            int maxSize = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (!visited[i]) {
+                    int start = nums[i], count = 0;
+                    do {
+                        start = nums[start];
+                        count++;
+                        visited[start] = true;
+                    } while (start != nums[i]);
+                    maxSize = Math.max(maxSize, count);
+                }
+            }
+            return maxSize;
+        }
+    }
+
+    // 超过时间限制不能pass
+    // time O(M^2)
+    // espace O(1)
+    class SetSolution {
+        public int arrayNesting(int[] nums) {
+            int maxSize = 0;
+            for (int i = 0; i < nums.length; i++) {
+                int startIndex = nums[i];
+                int count = 0;
+                do {
+                    count++;
+                    startIndex = nums[startIndex];
+                } while (startIndex != nums[i]);
+                maxSize = Math.max(count, maxSize);
+            }
+            return maxSize;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayNesting565 arrayNesting565 = new ArrayNesting565();
+        SetSolution setSolution = arrayNesting565.new SetSolution();
+        int[] nums = new int[] { 0, 2, 1 };
+        setSolution.arrayNesting(nums);
+    }
+}
---
+++ src/leetcode/datastructure/array/ToplitezMatrix766.java
@@ -0,0 +1,71 @@
+package leetcode.datastructure.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ToplitezMatrix766 {
+
+    // 对于对角线上的元素来说，如果当前元素不是第一个出现的元素，那么它前面的元素一定在当前元素的左上角。可以推出，对于位于坐标 (r, c) 上的元素
+    // 只需要检查 r == 0 OR c == 0 OR matrix[r-1][c-1] == matrix[r][c] 就可以了。
+    // time O(M*N)
+    // espace O(1)
+    class checkLeftNeigbor {
+        public boolean isToeplitzMatrix(int[][] matrix) {
+            for (int row = 0; row < matrix.length; row++) {
+                for (int col = 0; col < matrix[0].length; col++) {
+                    if (row > 0 && col > 0 && matrix[row - 1][col - 1] != matrix[row][col]) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+    // 思路 如果两个元素是对角线的元素则他们的行数-列数的差值相等
+    // time O(M*N)
+    // espace O(M+N)
+    class Diagnaol {
+        public boolean isToeplitzMatrix(int[][] matrix) {
+            Map<Integer, Integer> groups = new HashMap<>();
+            for (int r = 0; r < matrix.length; r++) {
+                for (int c = 0; c < matrix[0].length; c++) {
+                    if (!groups.containsKey(r - c)) {
+                        groups.put(r - c, matrix[r][c]);
+                    } else if (groups.get(r - c) != matrix[r][c]) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+    // 这种方法会很慢
+    class Recursive {
+        public boolean isToeplitzMatrix(int[][] matrix) {
+            for (int i = 0; i < matrix.length; i++) {
+                if (!isDiagonalEqual(matrix, i + 1, 0, matrix[i][0])) {
+                    return false;
+                }
+            }
+            for (int i = 0; i < matrix[0].length; i++) {
+                if (!isDiagonalEqual(matrix, 0, i + 1, matrix[0][i + 1])) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private boolean isDiagonalEqual(int[][] array, int r, int c, int target) {
+            if (r >= 0 && r < array.length && c >= 0 && c < array[0].length) {
+                if (array[r][c] != target) {
+                    return false;
+                } else {
+                    return isDiagonalEqual(array, r + 1, c + 1, target);
+                }
+            }
+            return true;
+        }
+    }
+}
