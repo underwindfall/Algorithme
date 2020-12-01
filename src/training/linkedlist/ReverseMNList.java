08cba7d02fbc42cdf2d512d752d05da7d31338bb
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-12-01 22:22:51 +0100

    翻转M到N的元素

---
+++ src/training/linkedlist/ReverseMNList.java
@@ -0,0 +1,36 @@
+package training.linkedlist;
+
+public class ReverseMNList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    ListNode successor = null;
+
+    private ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.next;
+            return head;
+        }
+        // 以 head.next 为起点，需要反转前 n - 1 个节点
+        ListNode last = reverseN(head.next, n - 1);
+        // head 的 下一个 的下一个是successor
+        head.next.next = head;
+        // 让反转之后的 head 节点和后面的节点连起来
+        head.next = successor;
+        return last;
+    }
+
+    ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverseN(head, n);
+        }
+        head.next = reverseBetween(head.next, m - 1, n - 1);
+        return head;
+    }
+}
