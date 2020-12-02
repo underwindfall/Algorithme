93661fd1228fd922f61c6caec73920e99f37e929
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-12-02 21:36:49 +0100

    [剑指] 单例模式

---
+++ src/leetcode/datastructure/linkdlist/ReverseNodesInKGroup25.java
@@ -0,0 +1,26 @@
+package leetcode.datastructure.linkdlist;
+
+public class ReverseNodesInKGroup25 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        
+    }
+
+}
