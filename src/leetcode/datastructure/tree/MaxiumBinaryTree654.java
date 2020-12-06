531ab694bc3e2cdb347de06642dbf1e26309f244
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-12-06 20:36:12 +0100

    Leetcode 654 maxium binary tree

---
+++ src/leetcode/datastructure/tree/MaxiumBinaryTree654.java
@@ -0,0 +1,38 @@
+package leetcode.datastructure.tree;
+
+public class MaxiumBinaryTree654 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // time O(N*logN)
+    // espace O(logN)
+    TreeNode constructMaximumBinaryTree(int nums[]) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int lo, int hi) {
+        if (lo > hi) {
+            return null;
+        }
+        int maxVal = Integer.MIN_VALUE;
+        int maxIndex = -1;
+        for (int i = lo; i <= hi; i++) {
+            if (nums[i] > maxVal) {
+                maxVal = nums[i];
+                maxIndex = i;
+            }
+        }
+        TreeNode root = new TreeNode(maxVal);
+        root.left = build(nums, lo, maxIndex - 1);
+        root.right = build(nums, maxIndex + 1, hi);
+        return root;
+    }
+
+}
