2ea4d170d5479b29dd02fc7676b28a75525347ee
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-12-08 02:13:09 +0100

    Leetcode 106 construct tree from inorder and postorder

---
+++ src/leetcode/datastructure/tree/In_Post_Order_BuildTree106.java
@@ -0,0 +1,39 @@
+package leetcode.datastructure.tree;
+
+// https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+public class In_Post_Order_BuildTree106 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // O(N)
+    // O(N)
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+
+    TreeNode build(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
+        if (inStart > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[postEnd]);
+        int index = 0;
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == root.val) {
+                index = i;
+                break;
+            }
+        }
+
+        int leftSize = index - inStart;
+        root.left = build(inorder, inStart, index - 1, postorder, postStart, postStart + leftSize - 1);
+        root.right = build(inorder, index + 1, inEnd, postorder, postStart + leftSize, postEnd - 1);
+        return root;
+    }
+}
