06467b07625dd5937d7db47814d24241ec40fbd5
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-12-11 00:24:08 +0100

    Leetcode 617 merge binary tree

---
+++ src/leetcode/datastructure/tree/MergeBinaryTrees617.java
@@ -0,0 +1,29 @@
+package leetcode.datastructure.tree;
+
+public class MergeBinaryTrees617 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // DFS
+    // time O(max(Heigh1,Heigh2))
+    // espace O(min(Heigh1,Heigh2))
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        TreeNode merged = new TreeNode(t1.val + t2.val);
+        merged.left = mergeTrees(t1.left, t2.left);
+        merged.right = mergeTrees(t1.right, t2.right);
+        return merged;
+    }
+}
