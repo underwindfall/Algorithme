1b4e9e607df4676bd501c188e46d72b4104bd2b7
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-12-11 01:08:06 +0100

    Leetcode 112 Path sum

---
+++ src/leetcode/datastructure/tree/PathSum112.java
@@ -0,0 +1,24 @@
+package leetcode.datastructure.tree;
+
+public class PathSum112 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // time O(N)
+    // espace O(logN)
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null && root.val == sum)
+            return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
