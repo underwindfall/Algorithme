d4d90f533d3d60a7efb6f0c1b4d1fa3277bebd7a
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-12-17 02:18:55 +0100

    Leetcode 513 find bottom left value

---
+++ src/leetcode/datastructure/tree/FindBottomLeftValue513.java
@@ -0,0 +1,46 @@
+package leetcode.datastructure.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// https://leetcode-cn.com/problems/find-bottom-left-tree-value/
+public class FindBottomLeftValue513 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // time O(logN)
+    // espace O(N)
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            root = queue.poll();
+            if (root.right != null) {
+                queue.add(root.right);
+            }
+            if (root.left != null) {
+                queue.add(root.left);
+            }
+        }
+        return root.val;
+    }
+}
