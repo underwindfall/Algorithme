ab67f03e066b8760cbedb5f37412de8d364bf0c5
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-12-20 15:15:33 +0100

    Leetcode 337 House Robber III

---
+++ src/leetcode/algo/dp/HouseRobberIII337.java
@@ -0,0 +1,26 @@
+package leetcode.algo.dp;
+
+// https://leetcode-cn.com/problems/house-robber-iii/
+public class HouseRobberIII337 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int rob(TreeNode root) {
+        if (root == null)
+            return 0;
+        int val1 = root.val;
+        if (root.left != null)
+            val1 += rob(root.left.left) + rob(root.left.right);
+        if (root.right != null)
+            val1 += rob(root.right.left) + rob(root.right.right);
+        int val2 = rob(root.left) + rob(root.right);
+        return Math.max(val1, val2);
+    }
+}
