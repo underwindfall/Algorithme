54cdeeb1b75698fe9c9f3b2ca6c92b3da08dd5d0
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-12-24 23:49:14 +0100

    Leetcode 20 isValid

---
+++ src/leetcode/datastructure/stack/IsValid20.java
@@ -0,0 +1,25 @@
+package leetcode.datastructure.stack;
+
+import java.util.Stack;
+
+// https://leetcode-cn.com/problems/valid-parentheses/
+public class IsValid20 {
+    public boolean isValid(String s) {
+        if (s.isEmpty())
+            return true;
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.empty() || c != stack.pop())
+                return false;
+        }
+        if (stack.empty())
+            return true;
+        return false;
+    }
+}
