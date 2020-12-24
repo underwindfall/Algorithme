bc03036f5bf0364e163c3b761673a373a841b1f4
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-12-24 21:41:14 +0100

    Leetcode 752 openlock

---
+++ src/leetcode/datastructure/queue/OpenLock752.java
@@ -0,0 +1,126 @@
+package leetcode.datastructure.queue;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+// https://leetcode-cn.com/problems/open-the-lock/
+// 思路通过BFS处理问题
+// 已知初识测试的code 为0000 + target目的地 0202 每次只能移动一步 求最短路径
+public class OpenLock752 {
+
+    String plusOne(String s, int j) {
+        char[] ch = s.toCharArray();
+        if (ch[j] == '9')
+            ch[j] = '0';
+        else
+            ch[j] += 1;
+        return new String(ch);
+    }
+
+    String minusOne(String s, int j) {
+        char[] ch = s.toCharArray();
+        if (ch[j] == '0')
+            ch[j] = '9';
+        else
+            ch[j] -= 1;
+        return new String(ch);
+    }
+
+    // BFS
+    public int openLock(String[] deadends, String target) {
+        // 记录需要跳过的死亡密码
+        Set<String> deads = new HashSet<>();
+        for (String s : deadends) {
+            deads.add(s);
+        }
+        // 记录已经穷举过的密码，防止走回头路
+        Set<String> visited = new HashSet<>();
+        // 从起点开始启动广度优先搜索
+        Queue<String> q = new LinkedList<>();
+        int step = 0;
+        q.offer("0000");
+        visited.add("0000");
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            /* 将当前队列中的所有节点向周围扩散 */
+            for (int i = 0; i < sz; i++) {
+                String cur = q.poll();
+                /* 判断是否到达终点 */
+                if (deads.contains(cur)) {
+                    continue;
+                }
+                if (cur.equals(target)) {
+                    return step;
+                }
+
+                /* 将一个节点的相邻节点加入队列 */
+                for (int j = 0; j < 4; j++) {
+                    String up = plusOne(cur, j);
+                    if (!visited.contains(up)) {
+                        q.add(up);
+                        visited.add(up);
+                    }
+                    String down = minusOne(cur, j);
+                    if (!visited.contains(down)) {
+                        q.add(down);
+                        visited.add(down);
+                    }
+                }
+            }
+            /* 在这里增加步数 */
+            step++;
+        }
+        return -1;
+    }
+
+    // 这里有一种取巧法，就是双向取这个target 即 从开始找到结尾，也可以从结尾找到开始
+    // 双向BFS
+    int doubleBFSopenLock(String[] deadends, String target) {
+        Set<String> deads = new HashSet<>();
+        for (String s : deadends)
+            deads.add(s);
+        // 用集合不用队列，可以快速判断元素是否存在
+        Set<String> q1 = new HashSet<>();
+        Set<String> q2 = new HashSet<>();
+        Set<String> visited = new HashSet<>();
+
+        int step = 0;
+        q1.add("0000");
+        q2.add(target);
+
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            // 哈希集合在遍历的过程中不能修改，用 temp 存储扩散结果
+            Set<String> temp = new HashSet<>();
+
+            /* 将 q1 中的所有节点向周围扩散 */
+            for (String cur : q1) {
+                /* 判断是否到达终点 */
+                if (deads.contains(cur))
+                    continue;
+                if (q2.contains(cur))
+                    return step;
+                visited.add(cur);
+
+                /* 将一个节点的未遍历相邻节点加入集合 */
+                for (int j = 0; j < 4; j++) {
+                    String up = plusOne(cur, j);
+                    if (!visited.contains(up))
+                        temp.add(up);
+                    String down = minusOne(cur, j);
+                    if (!visited.contains(down))
+                        temp.add(down);
+                }
+            }
+            /* 在这里增加步数 */
+            step++;
+            // temp 相当于 q1
+            // 这里交换 q1 q2，下一轮 while 就是扩散 q2
+            q1 = q2;
+            q2 = temp;
+        }
+        return -1;
+    }
+
+}
