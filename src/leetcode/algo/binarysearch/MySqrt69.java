8356035e85c6e4a755d82395abfbb92748282883
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-01-06 17:04:39 +0100

    Leetcode 69 MySqrt

---
+++ src/leetcode/algo/binarysearch/MySqrt69.java
@@ -0,0 +1,20 @@
+package leetcode.algo.binarysearch;
+
+// https://leetcode-cn.com/problems/sqrtx/
+public class MySqrt69 {
+    public int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+        int ans = -1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
