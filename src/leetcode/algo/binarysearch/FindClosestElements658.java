c2b3da5f21b00ef0496df60f7340f61d3d20276e
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-01-09 00:08:40 +0100

    Leetcode 367 perfect sqaure

---
+++ src/leetcode/algo/binarysearch/FindClosestElements658.java
@@ -0,0 +1,12 @@
+package leetcode.algo.binarysearch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode-cn.com/problems/find-k-closest-elements/
+public class FindClosestElements658 {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> ans = new ArrayList<>();
+        return ans;
+    }
+}
