c618704a43d029e07e68a7ef6af17fc50b3a87c9
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-02-02 23:36:07 +0100

    Leetcode 818 racecar bfs解法

--- src/leetcode/algo/dp/subsquence/RaceCar818.java
+++ src/leetcode/algo/dp/subsquence/RaceCar818.java
@@ -1,5 +1,10 @@
 package leetcode.algo.dp.subsquence;
 
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
 // https://leetcode-cn.com/problems/race-car/
 public class RaceCar818 {
     // 思路
@@ -44,5 +49,43 @@ public class RaceCar818 {
             }
             return dp[target];
         }
+    }
+
+    //把他想像成从root开始有无数个选择的可能性
+    //position 0 开始 有开始做出判断
+    public int racecar(int target) {
+        Deque<int[]> queue = new LinkedList<>();
+        queue.offerLast(new int[] {0, 1}); // starts from position 0 with speed 1
+        
+        Set<String> visited = new HashSet<>();
+        visited.add(0 + " " + 1);
+        
+        for (int level = 0; !queue.isEmpty(); level++) {
+            for(int k = queue.size(); k > 0; k--) {
+                int[] cur = queue.pollFirst();  // cur[0] is position; cur[1] is speed
+                
+                if (cur[0] == target) {
+                    return level;
+                }
+                
+                int[] nxt = new int[] {cur[0] + cur[1], cur[1] << 1};  // accelerate instruction
+                String key = (nxt[0] + " " + nxt[1]);
+                
+                if (!visited.contains(key) && 0 < nxt[0] && nxt[0] < (target << 1)) {
+                    queue.offerLast(nxt);
+                    visited.add(key);
+                }
+                
+                nxt = new int[] {cur[0], cur[1] > 0 ? -1 : 1};  // reverse instruction
+                key = (nxt[0] + " " + nxt[1]);
+                
+                if (!visited.contains(key) && 0 < nxt[0] && nxt[0] < (target << 1)) {
+                    queue.offerLast(nxt);
+                    visited.add(key);
+                }
+            }
+        }
+        
+        return -1;
     }
 }
