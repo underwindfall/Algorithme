56f09e7e21d92f6d8ab65075696fa15f29531817
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-02-03 23:57:03 +0100

    Leetcode 300 贪心+二分

--- src/leetcode/algo/dp/subsquence/LengthOfLIS300.java
+++ src/leetcode/algo/dp/subsquence/LengthOfLIS300.java
@@ -22,4 +22,32 @@ public class LengthOfLIS300 {
             return maxAns;
         }
     }
+
+    //重复计算过多所以不适合
+    class Recursive {
+        public int lengthOfLIS(int[] nums) {
+            if (nums == null) {
+                return 0;
+            }
+            return dfs(-1, 0, nums);
+        }
+
+        private int dfs(int pre, int cur, int[] nums) {
+            if (cur == nums.length) {
+                return 0;
+            }
+            int a = 0;
+            int b = 0;
+            // pre小于0是初始状态，继续往后判断
+            // if条件满足说明是上升子序列，长度要+1
+            if (pre < 0 || nums[pre] < nums[cur]) {
+                a = dfs(cur, cur + 1, nums) + 1;
+            }
+            // 如果不满足可能是不满足上升子序列条件
+            // 也可能是 满足条件但主动放弃
+            b = dfs(pre, cur + 1, nums);
+            return Math.max(a, b);
+        }
+
+    }
 }
