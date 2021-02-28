0de32384050f887f8449c4b77ce442922c5601da
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-02-28 21:25:33 +0100

    Leetcode 256 paint house dp

--- src/leetcode/algo/dp/twodimension/PaintHouse256.java
+++ src/leetcode/algo/dp/twodimension/PaintHouse256.java
@@ -6,7 +6,29 @@ import java.util.Map;
 // https://leetcode-cn.com/problems/paint-house/
 public class PaintHouse256 {
 
-   
+    // bottom up
+    class DP {
+        public int minCost(int[][] costs) {
+            // 子问题
+            // guess
+            // 子问题的关系
+            // dp 当前的子问题是 每个房子的刷油漆后最小的价格
+            // dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j + 1]) + costs[i][j]
+            if (costs.length == 0)
+                return 0;
+            int[][] dp = new int[costs.length][3];
+            for (int i = 0; i < 3; i++) {
+                // set initial value
+                dp[costs.length - 1][i] = costs[costs.length - 1][i];
+            }
+            for (int i = costs.length - 2; i >= 0; i--) {
+                dp[i][0] = Math.min(dp[i + 1][1], dp[i + 1][2]) + costs[i][0];
+                dp[i][1] = Math.min(dp[i + 1][0], dp[i + 1][2]) + costs[i][1];
+                dp[i][2] = Math.min(dp[i + 1][0], dp[i + 1][1]) + costs[i][2];
+            }
+            return Math.min(Math.min(dp[0][0], dp[0][1]), dp[0][2]);
+        }
+    }
 
     class RecursiveMemo {
         private int[][] costs;
