376e86f4dc40143fe03d14681f948f9b0ba3f4ec
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-03-02 01:24:54 +0100

    Leetcode 887 super egg drop

--- src/leetcode/algo/dp/twodimension/SuperEggDrop887.java
+++ src/leetcode/algo/dp/twodimension/SuperEggDrop887.java
@@ -1,7 +1,48 @@
 package leetcode.algo.dp.twodimension;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // https://leetcode-cn.com/problems/super-egg-drop
 public class SuperEggDrop887 {
+    // https://leetcode-cn.com/problems/super-egg-drop/solution/ji-dan-diao-luo-xiang-jie-by-shellbye/
+    class BinaryDP {
+
+        Map<Integer, Integer> cache = new HashMap<>();
+
+        public int superEggDrop(int K, int N) {
+            if (N == 0)
+                return 0;
+            else if (K == 1)
+                return N;
+
+            Integer key = N * 1000 + K; // K <= 100
+            if (cache.containsKey(key))
+                return cache.get(key);
+
+            int low = 1, high = N;
+            while (low + 1 < high) {
+                int middle = (low + high) / 2;
+                int lowVal = superEggDrop(K - 1, middle - 1);
+                int highVal = superEggDrop(K, N - middle);
+
+                if (lowVal < highVal)
+                    low = middle;
+                else if (lowVal > highVal)
+                    high = middle;
+                else
+                    low = high = middle;
+            }
+            int minimum = 1 + Math.min(Math.max(superEggDrop(K - 1, low - 1), superEggDrop(K, N - low)),
+                    Math.max(superEggDrop(K - 1, high - 1), superEggDrop(K, N - high)));
+
+            cache.put(key, minimum);
+
+            return cache.get(key);
+        }
+
+    }
+
     // bottom up
     class DP {
         public int superEggDrop(int K, int N) {
