d964be0cac2f0b33451b01125235aa0cef113629
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-03-04 21:21:54 +0100

    Leetcode 403 can cross dp

--- src/leetcode/algo/dp/twodimension/CanCross403.java
+++ src/leetcode/algo/dp/twodimension/CanCross403.java
@@ -1,9 +1,94 @@
 package leetcode.algo.dp.twodimension;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 // https://leetcode-cn.com/problems/frog-jump/
 public class CanCross403 {
+    // 这个dp设计的很巧妙
+    // 这个map 是 key 是当前石头index，value 是所有的jumpsize的集合
+    // 如果当前存在一个value 表明当前的到达的位置则返回则更新这个值
+    class DP {
+        public class Solution {
+            public boolean canCross(int[] stones) {
+                HashMap<Integer, Set<Integer>> map = new HashMap<>();
+                for (int i = 0; i < stones.length; i++) {
+                    map.put(stones[i], new HashSet<Integer>());
+                }
+                map.get(0).add(0);
+                for (int i = 0; i < stones.length; i++) {
+                    //当前的jump size
+                    for (int k : map.get(stones[i])) {
+                        for (int step = k - 1; step <= k + 1; step++) {
+                            //代表目标所能到达的位置
+                            if (step > 0 && map.containsKey(stones[i] + step)) {
+                                map.get(stones[i] + step).add(step);
+                            }
+                        }
+                    }
+                }
+                return map.get(stones[stones.length - 1]).size() > 0;
+            }
+        }
+
+    }
+
+    class Dfs {
+        public boolean canCross(int[] stones) {
+            // 从0开始起跳，目前每次跳0步
+            return canCross(stones, 0, 0);
+        }
+
+        // 上一步跳了jumpSize步，来到current处，基于此，能否到达终点
+        boolean canCross(int[] stones, int currentPos, int jumpSize) {
+            /// 枚举出可跳的选择，从下一个石头到最后一个
+            for (int i = currentPos + 1; i < stones.length; i++) {
+                // 本轮迭代选择跳到i，算出第i石头到当前石头的距离
+                int gap = stones[i] - stones[currentPos];
+                // 代表可以跨过 i
+                if (gap >= jumpSize - 1 && gap <= jumpSize + 1) {
+                    // 如果 基于此的递归返回true，说明能到终点
+                    if (canCross(stones, i, gap)) {
+                        return true;// 递归压栈压到底，如果true了，则true一层向上返回
+                    }
+                }
+            }
+            // 考察完当前的所有选项，看看来到的index是不是终点
+            return currentPos == stones.length - 1;
+        }
+    }
+
+    class DfsPlusMemo {
+        public boolean canCross(int[] stones) {
+            // map代表着这里检查过了
+            Map<Integer, Boolean> map = new HashMap<>();
+            return canCross(stones, 0, 0, map);
+        }
+
+        boolean canCross(int[] stones, int currentPos, int jumpSize, Map<Integer, Boolean> map) {
+            int key = currentPos * 1000 + jumpSize;
+            if (map.containsKey(key)) {
+                return map.get(key);
+            }
+            for (int i = currentPos + 1; i < stones.length; i++) {
+                int gap = stones[i] - stones[currentPos];
+                // 代表可以跨过 i
+                if (gap >= jumpSize - 1 && gap <= jumpSize + 1) {
+                    if (canCross(stones, i, gap, map)) {
+                        // 这里更新的是新的key
+                        map.put(i * 1000 + gap, true);
+                        return true;
+                    }
+                }
+            }
+            map.put(key, currentPos == stones.length - 1);
+            return currentPos == stones.length - 1;
+        }
+    }
+
     public class Solution {
         public boolean canCross(int[] stones) {
             return can_Cross(stones, 0, 0);
