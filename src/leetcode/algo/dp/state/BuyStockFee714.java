cbe528c254894b344e0aab69db4354ff7d9a9b99
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-03-13 16:50:54 +0100

    Leetcode buystockfee 714

---
+++ src/leetcode/algo/dp/state/BuyStockFee714.java
@@ -0,0 +1,17 @@
+package leetcode.algo.dp.state;
+
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+public class BuyStockFee714 {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+}
