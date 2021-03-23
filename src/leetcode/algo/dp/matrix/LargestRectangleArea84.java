779c2c6675be826dddce8972b838919d230707b4
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-03-23 12:46:21 +0100

    Leetcode 84 单调站

--- src/leetcode/algo/dp/matrix/LargestRectangleArea84.java
+++ src/leetcode/algo/dp/matrix/LargestRectangleArea84.java
@@ -1,5 +1,7 @@
 package leetcode.algo.dp.matrix;
 
+import java.util.Stack;
+
 // https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
 public class LargestRectangleArea84 {
     // 单调站
@@ -29,5 +31,37 @@ public class LargestRectangleArea84 {
         for (int i = 0; i < n; i++)
             res = Math.max(res, (right_i[i] - left_i[i] - 1) * heights[i]);
         return res;
+    }
+
+    class IncreStack{
+        public int largestRectangleArea(int[] heights) {
+            int n = heights.length;
+            int[] left = new int[n];
+            int[] right = new int[n];
+            
+            Stack<Integer> mono_stack = new Stack<Integer>();
+            for (int i = 0; i < n; ++i) {
+                while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                    mono_stack.pop();
+                }
+                left[i] = (mono_stack.isEmpty() ? -1 : mono_stack.peek());
+                mono_stack.push(i);
+            }
+    
+            mono_stack.clear();
+            for (int i = n - 1; i >= 0; --i) {
+                while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                    mono_stack.pop();
+                }
+                right[i] = (mono_stack.isEmpty() ? n : mono_stack.peek());
+                mono_stack.push(i);
+            }
+            
+            int ans = 0;
+            for (int i = 0; i < n; ++i) {
+                ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+            }
+            return ans;
+        }
     }
 }
