1bc79819bc4033f6f0376c873cda16112dcb068c
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-03-25 10:02:29 +0100

    Leetcode 303 range sum array

---
+++ src/leetcode/algo/dp/prefix/RangeSumArray303.java
@@ -0,0 +1,19 @@
+package leetcode.algo.dp.prefix;
+
+// https://zhuanlan.zhihu.com/p/339237818
+public class RangeSumArray303 {
+    // 前缀和 基本概念
+    // [i, j] 的和 包含i，j 数学上的算法是 prefix(j + 1) - prefix(i)
+    private int[] prefixSum;
+
+    public RangeSumArray303(int[] nums) {
+        prefixSum = new int[nums.length  + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum[i+1] = prefixSum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return prefixSum[j + 1] - prefixSum[i];
+    }
+}
