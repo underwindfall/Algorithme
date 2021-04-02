94aeb05bb4b7e9c53b3870edf6a36e803e4efcef
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-04-02 09:42:44 +0200

    Leetcode 152 maximumProduct subarray

--- src/leetcode/algo/dp/subarray/MaximumProductSubArray152.java
+++ src/leetcode/algo/dp/subarray/MaximumProductSubArray152.java
@@ -2,6 +2,25 @@ package leetcode.algo.dp.subarray;
 
 // https://leetcode-cn.com/problems/maximum-product-subarray/
 public class MaximumProductSubArray152 {
+
+    class NegativePositveDp {
+        public int maxProduct(int[] nums) {
+            int max = Integer.MIN_VALUE, imax = 1, imin = 1;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] < 0) {
+                    int tmp = imax;
+                    imax = imin;
+                    imin = tmp;
+                }
+                imax = Math.max(imax * nums[i], nums[i]);
+                imin = Math.min(imin * nums[i], nums[i]);
+
+                max = Math.max(max, imax);
+            }
+            return max;
+        }
+    }
+
     class DP {
 
         public int maxProduct(int[] nums) {
