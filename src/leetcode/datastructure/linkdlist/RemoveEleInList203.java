839d8e37d9c1bfc009f98b9ffec67f35a5a199e9
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-04-11 15:01:25 +0200

    Leetocde 203 remve element list

---
+++ src/leetcode/datastructure/linkdlist/RemoveEleInList203.java
@@ -0,0 +1,38 @@
+package leetcode.datastructure.linkdlist;
+
+// https://leetcode-cn.com/problems/remove-linked-list-elements/
+public class RemoveEleInList203 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode prev = dummy, curr = head;
+
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
