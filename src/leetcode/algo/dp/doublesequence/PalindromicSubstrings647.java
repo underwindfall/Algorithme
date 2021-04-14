10237cfaf2d178892d7228e631431717b7290504
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-04-14 09:38:37 +0200

    Leetcode 647 PalindromicSubstring expandCenter解法

--- src/leetcode/algo/dp/doublesequence/PalindromicSubstrings647.java
+++ src/leetcode/algo/dp/doublesequence/PalindromicSubstrings647.java
@@ -23,5 +23,23 @@ public class PalindromicSubstrings647 {
         }
     }
 
-    
+    class ExpandCenter {
+        //想清楚 left是一直变化的所以只要满足条件的 num++就会别叠加
+        public int countSubstrings(String s) {
+            int num = 0;
+            int n = s.length();
+
+            // 遍历回文中心点
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j <= 1; j++) {// j=0,中心是一个点，j=1,中心是两个点
+                    int l = i;
+                    int r = i + j;
+                    while (l >= 0 && r < n && s.charAt(l--) == s.charAt(r++)) {
+                        num++;
+                    }
+                }
+            }
+            return num;
+        }
+    }
 }
