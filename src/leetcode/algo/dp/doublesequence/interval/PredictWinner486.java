f8a02593c71629cee67984f11dfd9d1b5eb92f0f
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-05-05 22:32:51 +0200

    Leetcode 486 === 877

---
+++ src/leetcode/algo/dp/doublesequence/interval/PredictWinner486.java
@@ -0,0 +1,55 @@
+package leetcode.algo.dp.doublesequence.interval;
+
+import java.util.Arrays;
+
+//https://leetcode-cn.com/problems/predict-the-winner/
+//跟Leetcode 877基本上一摸一样
+public class PredictWinner486 {
+
+    // dp(i, j) 定义为 （i，j） 区间内能够获取的最大净胜分数 他是一切的核心
+    // dp(i, j ) = max(nums[i] - dp(i + 1, j), nums[j] - dp(i, j - 1) )
+    class DP {
+        public boolean PredictTheWinner(int[] nums) {
+            int n = nums.length;
+            int[][] dp = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                dp[i][i] = nums[i];
+            }
+
+            // 对角线遍历表
+            for (int length = 1; length < n; length++) {
+                for (int i = 0; i + length < n; i++) {
+                    int j = i + length;
+                    dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+                }
+            }
+            return dp[0][n - 1] > 0;
+        }
+    }
+
+    public boolean PredictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] memo = new int[n][n];
+        for (int[] row : memo) {
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+        return dfs(nums, 0, n - 1, memo) >= 0;
+    }
+
+    // 计算的是绝对差值
+    int dfs(int[] piles, int left, int right, int[][] memo) {
+        if (left == right) {
+            return piles[left];
+        }
+        if (memo[left][right] != Integer.MIN_VALUE) {
+            return memo[left][right];
+        }
+        int chooseLeft = piles[left] - dfs(piles, left + 1, right, memo);
+        int chooseRight = piles[right] - dfs(piles, left, right - 1, memo);
+
+        // max 每次大家都是取相比下一层之差更大的
+        int ans = Math.max(chooseLeft, chooseRight);
+        memo[left][right] = ans;
+        return ans;
+    }
+}
