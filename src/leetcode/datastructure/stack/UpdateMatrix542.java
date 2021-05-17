30ebea6daca3d8bede004df795f5cfeab3924ee1
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-05-18 00:18:30 +0200

    Leetcode 542 update matrix BFS

--- src/leetcode/datastructure/stack/UpdateMatrix542.java
+++ src/leetcode/datastructure/stack/UpdateMatrix542.java
@@ -139,51 +139,60 @@ public class UpdateMatrix542 {
         }
     }
 
-    // time
-    // espace
+    // time O(M*N)
+    // espace (M*N)
     class Solution {
-        public int[][] updateMatrix(int[][] matrix) {
-            // 首先将所有的 0 都入队，并且将 1 的位置设置成 -1，表示该位置是 未被访问过的 1
-            Queue<int[]> q = new LinkedList<int[]>();
-            int n = matrix.length, m = matrix[0].length;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (matrix[i][j] == 0) {
-                        q.offer(new int[] { i, j });
+        // 首先将所有的 0 都入队，并且将 1 的位置设置成 -1，表示该位置是 未被访问过的 1
+        // bfs
+        public int[][] updateMatrix(int[][] mat) {
+            // bfs
+            // 和leetcode 994 类似
+            int M = mat.length, N = mat[0].length;
+            Queue<int[]> queue = new LinkedList<>();
+
+            for (int r = 0; r < M; r++) {
+                for (int c = 0; c < N; c++) {
+                    if (mat[r][c] == 0) {
+                        queue.add(new int[] { r, c });
                     } else {
-                        matrix[i][j] = -1;
+                        mat[r][c] = -1;
                     }
                 }
             }
-            bfs(matrix, q);
-            return matrix;
+
+            while (!queue.isEmpty()) {
+                int n = queue.size();
+                for (int i = 0; i < n; i++) {
+                    int[] node = queue.poll();
+                    int row = node[0], col = node[1];
+
+                    if (isValid(mat, row + 1, col) && mat[row + 1][col] == -1) {
+                        mat[row + 1][col] = mat[row][col] + 1;
+                        queue.add(new int[] { row + 1, col });
+                    }
+
+                    if (isValid(mat, row - 1, col) && mat[row - 1][col] == -1) {
+                        mat[row - 1][col] = mat[row][col] + 1;
+                        queue.add(new int[] { row - 1, col });
+                    }
+
+                    if (isValid(mat, row, col + 1) && mat[row][col + 1] == -1) {
+                        mat[row][col + 1] = mat[row][col] + 1;
+                        queue.add(new int[] { row, col + 1 });
+                    }
+
+                    if (isValid(mat, row, col - 1) && mat[row][col - 1] == -1) {
+                        mat[row][col - 1] = mat[row][col] + 1;
+                        queue.add(new int[] { row, col - 1 });
+                    }
+                }
+            }
+
+            return mat;
         }
 
-        void bfs(int[][] matrix, Queue<int[]> q) {
-            int n = matrix.length, m = matrix[0].length;
-            while (!q.isEmpty()) {
-                int sz = q.size();
-                for (int i = 0; i < sz; i++) {
-                    int[] node = q.poll();
-                    int r = node[0], c = node[1];
-                    if (r + 1 >= 0 && r + 1 < n && c >= 0 && c < m && matrix[r + 1][c] == -1) {
-                        matrix[r + 1][c] = matrix[r][c] + 1;
-                        q.offer(new int[] { r + 1, c });
-                    }
-                    if (r - 1 >= 0 && r - 1 < n && c >= 0 && c < m && matrix[r - 1][c] == -1) {
-                        matrix[r - 1][c] = matrix[r][c] + 1;
-                        q.offer(new int[] { r - 1, c });
-                    }
-                    if (r >= 0 && r < n && c + 1 >= 0 && c + 1 < m && matrix[r][c + 1] == -1) {
-                        matrix[r][c + 1] = matrix[r][c] + 1;
-                        q.offer(new int[] { r, c + 1 });
-                    }
-                    if (r >= 0 && r < n && c - 1 >= 0 && c - 1 < m && matrix[r][c - 1] == -1) {
-                        matrix[r][c - 1] = matrix[r][c] + 1;
-                        q.offer(new int[] { r, c - 1 });
-                    }
-                }
-            }
+        boolean isValid(int[][] matrix, int row, int col) {
+            return row >= 0 && row < matrix.length && col >= 0 && col < matrix[0].length;
         }
     }
 }
