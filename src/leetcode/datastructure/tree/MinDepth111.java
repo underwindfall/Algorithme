5eb6b79b5de0df63df2129ac0d2e2a90f2fd81d2
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-05-26 09:29:10 +0200

    Leetcode minDepth 111

--- src/leetcode/datastructure/tree/MinDepth111.java
+++ src/leetcode/datastructure/tree/MinDepth111.java
@@ -1,6 +1,7 @@
 package leetcode.datastructure.tree;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 // https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/description/
 public class MinDepth111 {
@@ -24,17 +25,17 @@ public class MinDepth111 {
     }
 
     // time O(N)
-    // espace O(logN)
+    // espace O(logN) H 树的高度
     class Dfs {
         public int minDepth(TreeNode root) {
             if (root == null) {
                 return 0;
             }
-
+            //左孩子和有孩子都为空的情况，说明到达了叶子节点，直接返回1即可
             if (root.left == null && root.right == null) {
                 return 1;
             }
-
+            //如果左孩子和右孩子其中一个为空，那么需要返回比较大的那个孩子的深度
             int min_depth = Integer.MAX_VALUE;
             if (root.left != null) {
                 min_depth = Math.min(minDepth(root.left), min_depth);
@@ -42,7 +43,7 @@ public class MinDepth111 {
             if (root.right != null) {
                 min_depth = Math.min(minDepth(root.right), min_depth);
             }
-
+            //3.最后一种情况，也就是左右孩子都不为空，返回最小深度+1即可
             return min_depth + 1;
         }
     }
@@ -56,21 +57,21 @@ public class MinDepth111 {
             if (root == null) {
                 return 0;
             }
-            LinkedList<TreeNode> queue = new LinkedList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
             queue.add(root);
             int count = 1;
             while (!queue.isEmpty()) {
                 int size = queue.size();
                 for (int i = 0; i < size; i++) {
-                    TreeNode node = queue.removeFirst();
+                    TreeNode node = queue.poll();
                     if (node.left == null && node.right == null) {
                         return count;
                     }
                     if (node.left != null) {
-                        queue.addLast(node.left);
+                        queue.add(node.left);
                     }
                     if (node.right != null) {
-                        queue.addLast(node.right);
+                        queue.add(node.right);
                     }
                 }
                 count++;
