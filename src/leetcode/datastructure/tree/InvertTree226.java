3eba2598aef1a08c755823a8545c9f13126a7bbd
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-05-26 23:04:04 +0200

    Leetcode 226 invert tree

--- src/leetcode/datastructure/tree/InvertTree226.java
+++ src/leetcode/datastructure/tree/InvertTree226.java
@@ -1,5 +1,8 @@
 package leetcode.datastructure.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 // https://leetcode-cn.com/problems/invert-binary-tree/
 public class InvertTree226 {
     public class TreeNode {
@@ -12,6 +15,7 @@ public class InvertTree226 {
         }
     }
 
+    // dfs
     // 思路 可以看到翻转二叉树就是把每个节点的左子树和右子树进行翻转
     // time O(N) N= num of tree node
     // espace O(logN) n=height of tree
@@ -24,5 +28,36 @@ public class InvertTree226 {
         root.left = newRightNode;
         root.right = newLeftNode;
         return root;
+    }
+
+    // bfs
+    // time O(N)
+    // espace O(N)
+    class BFS {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return root;
+            }
+            // 二叉树入队列，在迭代处理队列中的元素
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                // 每次都从队列中拿出一个节点 并交换左右子树
+                TreeNode tmp = queue.poll();
+                TreeNode left = tmp.left;
+                tmp.left = tmp.right;
+                tmp.right = left;
+
+                // 如果当前左子树不为空入队列处理
+                if (tmp.left != null) {
+                    queue.offer(tmp.left);
+                }
+                // 同理
+                if (tmp.right != null) {
+                    queue.offer(tmp.right);
+                }
+            }
+            return root;
+        }
     }
 }
