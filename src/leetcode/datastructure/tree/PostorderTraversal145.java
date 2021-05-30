7d0b8f5aaa316f5c928a9476fcfc5065c595cb6b
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-05-30 14:40:34 +0200

    Leetcode 145 postorder traversal

--- src/leetcode/datastructure/tree/PostorderTraversal145.java
+++ src/leetcode/datastructure/tree/PostorderTraversal145.java
@@ -26,6 +26,8 @@ public class PostorderTraversal145 {
         }
     }
 
+    // time O(N)
+    // espace O(N)
     class Recursive {
         List<Integer> result = new ArrayList<>();
 
@@ -44,21 +46,51 @@ public class PostorderTraversal145 {
         }
     }
 
+    // time O(N)
+    // espace O(N)
+    // 后序遍历 left, right, root -> reverse -> root, right, left
+    // 前序遍历 root, left , right -> 前序遍历 先进右节点 再进左节点
     class Iterative {
         public List<Integer> postorderTraversal(TreeNode root) {
             List<Integer> result = new ArrayList<>();
             Stack<TreeNode> stack = new Stack<>();
-            stack.push(root.left);
+            stack.push(root);
             while (!stack.isEmpty()) {
                 TreeNode node = stack.pop();
                 if (node == null)
                     continue;
-                stack.push(node.right);
-                stack.push(node.left);
                 result.add(node.val);
+                stack.push(node.left);
+                stack.push(node.right);
             }
-             Collections.reverse(result);
-             return result;
+            Collections.reverse(result);
+            return result;
+        }
+    }
+
+    // time O(N)
+    // espace O(N)
+    class Another {
+        public List<Integer> postorderTraversal(TreeNode root) {
+            List<Integer> result = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode prev = null;
+            while (!stack.isEmpty() || root != null) {
+                while (root != null) {
+                    stack.push(root);
+                    root = root.left;
+                }
+                root = stack.pop();
+                if (root.right == null || root.right == prev) {
+                    result.add(root.val);
+                    prev = root;
+                    root = null;
+                } else {
+                    stack.add(root);
+                    root = root.right;
+                }
+            }
+            return result;
         }
     }
 }
