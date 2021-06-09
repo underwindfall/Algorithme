6348f6eb68f94fd6809fca398e6b93984a968f79
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-06-09 23:11:24 +0200

    Leetcode 90 subsetsII

---
+++ src/leetcode/datastructure/backtrack/SubsetsII90.java
@@ -0,0 +1,31 @@
+package leetcode.datastructure.backtrack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import java.util.List;
+
+//https://leetcode-cn.com/problems/subsets-ii/
+public class SubsetsII90 {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        Arrays.sort(nums);
+        dfs(nums, 0, path, result);
+        return result;
+    }
+
+
+    void dfs(int[] nums, int startIndex, List<Integer> path, List<List<Integer>> result) {
+        result.add(new ArrayList<>(path));
+
+        for (int i = startIndex; i < nums.length; i++) {
+            if (i > startIndex && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            path.add(nums[i]);
+            dfs(nums, i + 1, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+}
