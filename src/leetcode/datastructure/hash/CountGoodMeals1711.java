94505124d442f363d52b1af6d24fe9434217678e
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-07-30 15:09:16 +0200

    Leetcode 1711 count good meals

---
+++ src/leetcode/datastructure/hash/CountGoodMeals1711.java
@@ -0,0 +1,30 @@
+package leetcode.datastructure.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//https://leetcode-cn.com/problems/count-good-meals/
+public class CountGoodMeals1711 {
+    // time O(N*LogC)
+    // espace O(N)
+    public int countPairs(int[] deliciousness) {
+        final int MOD = 1000000007;
+        int maxVal = 0;
+        for (int val : deliciousness) {
+            maxVal = Math.max(maxVal, val);
+        }
+        int maxSum = maxVal * 2;
+        int pairs = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = deliciousness.length;
+        for (int i = 0; i < n; i++) {
+            int val = deliciousness[i];
+            for (int sum = 1; sum <= maxSum; sum = sum * 2) {
+                int count = map.getOrDefault(sum - val, 0);
+                pairs = (pairs + count) % MOD;
+            }
+            map.put(val, map.getOrDefault(val, 0) + 1);
+        }
+        return pairs;
+    }
+}
