1e7d48e18bc77a673f4ab4d4f613dea9e31c1022
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-08-25 15:35:59 +0200

    Leetcode 813 LargestSumOfAverages

--- src/leetcode/algo/dp/doublesequence/interval/PredictWinner486.java
+++ src/leetcode/algo/dp/doublesequence/interval/PredictWinner486.java
@@ -17,13 +17,13 @@ public class PredictWinner486 {
             }
 
             // 对角线遍历表
-            for (int length = 1; length < n; length++) {
-                for (int i = 0; i + length < n; i++) {
-                    int j = i + length;
+
+            for (int i = n - 2; i >= 0; i--) {
+                for (int j = i + 1; j < n; j++) {
                     dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
                 }
             }
-            return dp[0][n - 1] > 0;
+            return dp[0][n - 1] >= 0;
         }
     }
 
--- src/leetcode/algo/dp/twodimension/LargestSumOfAverages813.java
+++ src/leetcode/algo/dp/twodimension/LargestSumOfAverages813.java
@@ -1,11 +1,14 @@
 package leetcode.algo.dp.twodimension;
 
+import java.util.Arrays;
+
 // https://leetcode-cn.com/problems/largest-sum-of-averages/
 public class LargestSumOfAverages813 {
-    // represent a largest number who use i number divided by k group
+    // time O(k * n^2)
+    // space O(n)
+    // dp(i, k) ===== represent a largest number who use i number divided by k group
     // 分割k-1 次就是分割成了K组
-    // dp[i][k] = max(dp[j][k-1]+avg(j+1....i)) ==> the range of j should be like (j
-    // >= k-1 && j + 1 < i otherwise cannot divided by k-1 group)
+    // dp[i][k] = max(dp[j][k-1]+avg(j+1....i)) ==> the range of j should be like (j>= k-1 && j + 1 < i otherwise cannot divided by k-1 group)
     class DP {
         public double largestSumOfAverages(int[] A, int K) {
             int n = A.length;
@@ -37,6 +40,52 @@ public class LargestSumOfAverages813 {
         }
     }
 
+    class DFSMemo {
+        // dfs
+        public double largestSumOfAverages(int[] nums, int k) {
+            double[][] memo = new double[nums.length][k + 1];
+            for (double[] m : memo) {
+                Arrays.fill(m, -1.0);
+            }
+            return dfs(nums, 0, k, memo);
+        }
+
+        double dfs(int[] nums, int startIndex, int group, double[][] memo) {
+            if (startIndex < 0 || startIndex > nums.length - 1) {
+                return 0.0;
+            }
+
+            if (group == 0) {
+                return 0.0;
+            }
+            // 求当前的group的均值
+            if (group == 1) {
+                double sum = 0.0;
+                for (int i = startIndex; i < nums.length; i++) {
+                    sum += nums[i];
+                }
+                return Double.valueOf(sum / (nums.length - startIndex));
+            }
+
+            if (memo[startIndex][group] != -1.0) {
+                return memo[startIndex][group];
+            }
+            // group 多组
+            double sum = 0.0, res = 0.0;
+            for (int i = startIndex; i < nums.length; i++) {
+                sum += nums[i];
+                // 当前组的平均值
+                double avg = sum / (i - startIndex + 1);
+                // k - 1 group
+                double result = dfs(nums, i + 1, group - 1, memo);
+                res = Math.max(res, result + avg);
+            }
+            memo[startIndex][group] = res;
+            return res;
+        }
+    }
+
+    // TLE超时
     // A divide K group
     // A (K-1) group + A[0] ....
     // min A (K-1) group + A[0] ....
@@ -62,7 +111,7 @@ public class LargestSumOfAverages813 {
                 sum += A[i];
                 double avg = sum / (i - startIndex + 1);
                 // k-1 group
-                double result = dfs(A, startIndex + 1, group - 1);
+                double result = dfs(A, i + 1, group - 1);
                 res = Math.max(res, avg + result);
             }
             return res;
