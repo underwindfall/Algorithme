dda3a0f057cf01f431479d4bf16f07dfd6dd83d5
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-09-04 07:52:51 +0200

    DP LC 5

--- src/leetcode/datastructure/array/LongestCommonPrefix14.java
+++ src/leetcode/datastructure/array/LongestCommonPrefix14.java
@@ -26,9 +26,9 @@ public class LongestCommonPrefix14 {
     }
 
     /**
-     * 思想二 纵向的查找这些信息，并且通过比较二维数组的数据来找到最大公解
-     * 时间复杂度：O(mn)O(mn)，其中 mm 是字符串数组中的字符串的平均长度，nn 是字符串的数量。最坏情况下，字符串数组中的每个字符串的每个字符都会被比较一次
-     * espace: O(1)
+     * 思想二 纵向的查找这些信息，并且通过比较二维数组的数据来找到最大公解 时间复杂度：O(mn)O(mn)，其中 mm 是字符串数组中的字符串的平均长度，nn
+     * 是字符串的数量。最坏情况下，字符串数组中的每个字符串的每个字符都会被比较一次 espace: O(1)
+     * 
      * @param strs
      * @return
      */
@@ -42,21 +42,53 @@ public class LongestCommonPrefix14 {
             char c = strs[0].charAt(i);
             for (int j = 1; j < count; j++) {
                 if (i == strs[j].length() || strs[j].charAt(i) != c) {
-                    return strs[0].substring(0,i);
+                    return strs[0].substring(0, i);
                 }
             }
         }
         return strs[0];
     }
 
-    
+
+    class DvideAndConquer {
+        public String longestCommonPrefix(String[] strs) {
+            if (strs == null || strs.length == 0) {
+                return "";
+            } else {
+                return longestCommonPrefix(strs, 0, strs.length - 1);
+            }
+        }
+
+        public String longestCommonPrefix(String[] strs, int start, int end) {
+            if (start == end) {
+                return strs[start];
+            } else {
+                int mid = (end - start) / 2 + start;
+                String lcpLeft = longestCommonPrefix(strs, start, mid);
+                String lcpRight = longestCommonPrefix(strs, mid + 1, end);
+                return commonPrefix(lcpLeft, lcpRight);
+            }
+        }
+
+        public String commonPrefix(String lcpLeft, String lcpRight) {
+            int minLength = Math.min(lcpLeft.length(), lcpRight.length());
+            for (int i = 0; i < minLength; i++) {
+                if (lcpLeft.charAt(i) != lcpRight.charAt(i)) {
+                    return lcpLeft.substring(0, i);
+                }
+            }
+            return lcpLeft.substring(0, minLength);
+        }
+
+    }
+
     // 思路三 二分法解决
     // 拿第一个单词作为标准
     // 他的长度决定了公共开头的单词的length
     // 如果mid可以让每个单词都满足条件 mid+1
     // else mid -1
-    //time:O(logN*M*N) n 第一个单词长度
-    //espace:O(1)
+    // time:O(logN*M*N) n 第一个单词长度
+    // espace:O(1)
     public String longestCommonPrefixBinarySearch(String[] strs) {
         if (strs == null || strs.length == 0) {
             return "";
--- src/leetcode/datastructure/array/LongestPalindromeSubString5.java
+++ src/leetcode/datastructure/array/LongestPalindromeSubString5.java
@@ -2,32 +2,92 @@ package leetcode.datastructure.array;
 
 // https://leetcode-cn.com/problems/longest-palindromic-substring/
 public class LongestPalindromeSubString5 {
+    // time O(n^2)
+    // space O(n^2)
     // 动态规划
     // 如果P(i,j)存在回文串情况是
     // String的开始和结尾是同样的字符 + 其中出去开头结尾的子字节是也是回文串
     public String longestPalindrome(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        String ans = "";
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j + 1 < n; j++) {
-                int k = j + 1;
-                if (i == 0) {
-                    dp[j][k] = true;
-                } else if (i == 1) {
-                    dp[j][k] = (s.charAt(j) == s.charAt(k));
-                } else {
-                    dp[j][k] = (s.charAt(j) == s.charAt(k)) && dp[j + 1][k - 1];
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+        // dp[i][j] 表示 s[i..j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        // 初始化：所有长度为 1 的子串都是回文串
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        char[] charArray = s.toCharArray();
+        // 递推开始
+        // 先枚举子串长度
+        for (int L = 2; L <= len; L++) {
+            // 枚举左边界，左边界的上限设置可以宽松一些
+            for (int i = 0; i < len; i++) {
+                // 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+                int j = L + i - 1;
+                // 如果右边界越界，就可以退出当前循环
+                if (j >= len) {
+                    break;
                 }
 
-                if (dp[j][k] && i + 1 > ans.length()) {
-                    ans = s.substring(j, j + 1 + i);
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
                 }
             }
         }
-        return ans;
+        return s.substring(begin, begin + maxLen);
+
     }
 
+    // time O(n^2)
+    // space O(1)
+    class BruteForce {
+        public String longestExpandCenterPalindrome(String s) {
+            String ans = "";
+            int max = 0;
+            int len = s.length();
+            for (int i = 0; i < len; i++) {
+                for (int j = i + 1; j <= len; j++) {
+                    String test = s.substring(i, j);
+                    if (isPalindrome(test) && test.length() > max) {
+                        ans = s.substring(i, j);
+                        max = Math.max(max, ans.length());
+                    }
+                }
+            }
+            return ans;
+        }
+
+        boolean isPalindrome(String s) {
+            int len = s.length();
+            for (int i = 0; i < len / 2; i++) {
+                if (s.charAt(i) != s.charAt(len - i - 1)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    // time O(n^2)
+    // space O(1)
     public String longestExpandCenterPalindrome(String s) {
         if (s == null || s.length() < 1) {
             return "";
