2e485f9dd2eaa99bf416867c7959f821c325894e
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-09-07 15:50:32 +0200

    Leetcode 13 romantoInt

---
+++ src/leetcode/algo/string/RomanToInt13.java
@@ -0,0 +1,35 @@
+package leetcode.algo.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//https://leetcode-cn.com/problems/roman-to-integer/
+public class RomanToInt13 {
+    // time O(n)
+    // space O(n)
+    Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {
+        {
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }
+    };
+
+    public int romanToInt(String s) {
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int value = symbolValues.get(s.charAt(i));
+            if (i < n - 1 && value < symbolValues.get(s.charAt(i + 1))) {
+                ans -= value;
+            } else {
+                ans += value;
+            }
+        }
+        return ans;
+    }
+}
