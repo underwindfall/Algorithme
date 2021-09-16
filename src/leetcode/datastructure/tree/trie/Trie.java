bda6dc767e1b0a77a0b3e9332c77f734a928bcdf
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-09-16 16:16:49 +0200

    Tire

---
+++ src/leetcode/datastructure/tree/trie/Trie.java
@@ -0,0 +1,53 @@
+package leetcode.datastructure.tree.trie;
+
+//https://mp.weixin.qq.com/s?__biz=MzU4NDE3MTEyMA==&mid=2247488490&idx=1&sn=db2998cb0e5f08684ee1b6009b974089&chksm=fd9cb8f5caeb31e3f7f67dba981d8d01a24e26c93ead5491edb521c988adc0798d8acb6f9e9d&token=1232059512&lang=zh_CN#rd
+public class Trie {
+    // time O(len)
+    // space O(N*K)
+    class TrieNode {
+        boolean end;
+        TrieNode[] tns = new TrieNode[26];
+    }
+
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String s) {
+        TrieNode p = root;
+        for (int i = 0; i < s.length(); i++) {
+            int u = s.charAt(i) - 'a';
+            if (p.tns[u] == null) {
+                p.tns[u] = new TrieNode();
+            }
+            p = p.tns[u];
+        }
+        p.end = true;
+    }
+
+    public boolean search(String s) {
+        TrieNode p = root;
+        for (int i = 0; i < s.length(); i++) {
+            int u = s.charAt(i) - 'a';
+            if (p.tns[u] == null) {
+                return false;
+            }
+            p = p.tns[u];
+        }
+        return p.end;
+    }
+
+    public boolean startsWith(String s) {
+        TrieNode p = root;
+        for (int i = 0; i < s.length(); i++) {
+            int u = s.charAt(i) - 'a';
+            if (p.tns[u] == null) {
+                return false;
+            }
+            p = p.tns[u];
+        }
+        return true;
+    }
+}
