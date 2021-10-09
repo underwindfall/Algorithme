b305f6ee9a6ed69ef3f1d5084a896fea6d0241d8
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-10-09 22:14:58 +0200

    leetcode 374 GuessNumber

--- src/leetcode/algo/binarysearch/GuessNumber374.java
+++ src/leetcode/algo/binarysearch/GuessNumber374.java
@@ -2,11 +2,12 @@ package leetcode.algo.binarysearch;
 
 // https://leetcode-cn.com/problems/guess-number-higher-or-lower/
 public class GuessNumber374 {
+    
     public class Solution {
         public int guessNumber(int n) {
-            int left = 1;
-            int right = n;
-            while (left < right) {
+            int left = 1, right = n;
+            while (left <= right) {
+                //注意边界条件
                 int mid = (right - left) / 2 + left;
                 if (guess(mid) == 0) {
                     return mid;
@@ -16,8 +17,24 @@ public class GuessNumber374 {
                     left = mid + 1;
                 }
             }
-            return right;
+            return left;
         }
+
+        // public int guessNumber(int n) {
+        //     int left = 1;
+        //     int right = n;
+        //     while (left < right) {
+        //         int mid = (right - left) / 2 + left;
+        //         if (guess(mid) == 0) {
+        //             return mid;
+        //         } else if (guess(mid) == -1) {
+        //             right = mid - 1;
+        //         } else {
+        //             left = mid + 1;
+        //         }
+        //     }
+        //     return right;
+        // }
 
         private int guess(int num) {
             return -1;
