a5c78eec22599ce71eb3abab1cf9dfe3a485bd35
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-10-18 22:35:54 +0200

    leetcode 1277 CountSquares

---
+++ src/leetcode/algo/dp/matrix/CountSquares1277.java
@@ -0,0 +1,29 @@
+package leetcode.algo.dp.matrix;
+
+// https://leetcode-cn.com/problems/count-square-submatrices-with-all-ones/
+public class CountSquares1277 {
+    // time O(n*m)
+    // space O(n*m)
+    public int countSquares(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int m = matrix.length, n = matrix[0].length;
+        int[][] f = new int[m][n];
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0) {
+                    f[i][j] = matrix[i][j];
+                } else if (matrix[i][j] == 0) {
+                    f[i][j] = 0;
+                } else {
+                    f[i][j] = Math.min(Math.min(f[i][j - 1], f[i - 1][j]), f[i - 1][j - 1]) + 1;
+                }
+                ans += f[i][j];
+            }
+        }
+        return ans;
+    }
+}
