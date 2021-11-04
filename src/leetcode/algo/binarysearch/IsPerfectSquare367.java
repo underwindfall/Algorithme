3032f580c3cefa13ed29485dccbc4a043eb40c71
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-11-04 15:42:12 +0100

    leetcode 367 IsPerfectSquare

--- src/leetcode/algo/binarysearch/IsPerfectSquare367.java
+++ src/leetcode/algo/binarysearch/IsPerfectSquare367.java
@@ -3,19 +3,21 @@ package leetcode.algo.binarysearch;
 // https://leetcode-cn.com/problems/valid-perfect-square/
 public class IsPerfectSquare367 {
 
-        public boolean isPerfectSquare(int num) {
-            long left = 1, right = num;
-            while (left <= right) {
-                long mid = (right - left) / 2 + left;
-                if (mid * mid > num) {
-                    right = mid - 1;
-                } else if (mid * mid < num) {
-                    left = mid + 1;
-                } else {
-                    return true;
-                }
+    // time O(logN)
+    // space O(1)
+    public boolean isPerfectSquare(int num) {
+        long left = 1, right = num;
+        while (left <= right) {
+            long mid = (right - left) / 2 + left;
+            if (mid * mid > num) {
+                right = mid - 1;
+            } else if (mid * mid < num) {
+                left = mid + 1;
+            } else {
+                return true;
             }
-            return false;
         }
+        return false;
+    }
 
 }
