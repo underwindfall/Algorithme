08946c9c85944034737cbdc80da6d02b9faf7969
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-11-06 22:23:07 +0100

    leetcode1465 MaxArea

---
+++ src/leetcode/algo/greedy/MaxArea1465.java
@@ -0,0 +1,38 @@
+package leetcode.algo.greedy;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/
+public class MaxArea1465 {
+
+    // time O(nlogN + mLogM)
+    // space O(1)
+    // We will use long instead of int to prevent overflow
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        // Start by sorting the inputs
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        int n = horizontalCuts.length;
+        int m = verticalCuts.length;
+
+        // Consider the edges first
+        long maxHeight = Math.max(horizontalCuts[0], h - horizontalCuts[n - 1]);
+        for (int i = 1; i < n; i++) {
+            // horizontalCuts[i] - horizontalCuts[i - 1] represents the distance between
+            // two adjacent edges, and thus a possible height
+            maxHeight = Math.max(maxHeight, horizontalCuts[i] - horizontalCuts[i - 1]);
+        }
+
+        // Consider the edges first
+        long maxWidth = Math.max(verticalCuts[0], w - verticalCuts[m - 1]);
+        for (int i = 1; i < m; i++) {
+            // verticalCuts[i] - verticalCuts[i - 1] represents the distance between
+            // two adjacent edges, and thus a possible width
+            maxWidth = Math.max(maxWidth, verticalCuts[i] - verticalCuts[i - 1]);
+        }
+
+        // Be careful of overflow, and don't forget the modulo!
+        return (int) ((maxWidth * maxHeight) % (1000000007));
+    }
+
+}
