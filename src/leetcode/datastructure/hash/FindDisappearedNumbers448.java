9315b4c5f6b011f8799171de767d5e9d56045440
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-11-08 23:13:15 +0100

    leetcode 448 FindDisappearedNumbers

--- src/leetcode/datastructure/hash/FindDisappearedNumbers448.java
+++ src/leetcode/datastructure/hash/FindDisappearedNumbers448.java
@@ -1,7 +1,25 @@
 package leetcode.datastructure.hash;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/
 //cf 442
 public class FindDisappearedNumbers448 {
-    
+    // time O(n)
+    // space O(1)
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int pos = Math.abs(nums[i]) - 1;
+            nums[pos] = nums[pos] < 0 ? nums[pos] : -nums[pos];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+
+    }
 }
