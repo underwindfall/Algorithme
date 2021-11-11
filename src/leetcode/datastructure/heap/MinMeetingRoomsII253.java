5ce3c6f03fc1e37a29eef341277bd3403a32f65d
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-11-11 15:38:14 +0100

    leetcode 253 MinMeetingRoomsII

---
+++ src/leetcode/datastructure/heap/MinMeetingRoomsII253.java
@@ -0,0 +1,22 @@
+package leetcode.datastructure.heap;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+// https://leetcode.com/problems/meeting-rooms-ii/
+public class MinMeetingRoomsII253 {
+    // time O(n * logn)
+    // space O(n)
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(intervals[0][1]);
+        for (int i = 1; i < intervals.length; i++) {
+            if (pq.peek() <= intervals[i][0]) {
+                pq.poll();
+            }
+            pq.add(intervals[i][1]);
+        }
+        return pq.size();
+    }
+}
