47ec877e6007a5dcca1cfe425d75b4470bacbb98
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-11-23 21:30:23 +0100

    leetcode 549 LongestConsecutiveII

--- src/leetcode/datastructure/tree/LongestConsecutiveII549.java
+++ src/leetcode/datastructure/tree/LongestConsecutiveII549.java
@@ -4,6 +4,44 @@ package leetcode.datastructure.tree;
 public class LongestConsecutiveII549 {
     // time O(N)
     // espace O(N)
+    class DFS {
+        int max = 0;
+
+        public int longestConsecutive(TreeNode root) {
+            dfs(root);
+            return max;
+        }
+
+        int[] dfs(TreeNode root) {
+            // arr[0]为递增序列路径数，arr[1]为递减序列路径数
+            int[] arr = new int[2];
+            arr[0] = 1;
+            arr[1] = 1;
+            if (root == null)
+                return arr;
+            int[] left = dfs(root.left);
+            int[] right = dfs(root.right);
+            // dfs
+            if (root.left != null) {
+                if (root.left.val - 1 == root.val)
+                    arr[1] = left[1] + 1;
+                if (root.left.val + 1 == root.val)
+                    arr[0] = left[0] + 1;
+            }
+
+            // 判断root在右子树中处于递增还是递减，最后arr结果中保留最大值
+            if (root.right != null) {
+                if (root.right.val - 1 == root.val)
+                    arr[1] = Math.max(arr[1], right[1] + 1);
+                if (root.right.val + 1 == root.val)
+                    arr[0] = Math.max(arr[0], right[0] + 1);
+            }
+            // 统计结果
+            max = Math.max(max, arr[0] + arr[1] - 1);
+            return arr;
+        }
+    }
+
     public int longestConsecutive(TreeNode root) {
         if (root == null)
             return 0;
