3e87fb47e8bb3e1e911f60122f0f788f7e40819d
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-12-21 01:09:46 +0100

    leetcode MaximalSquare 221

--- src/leetcode/algo/dp/matrix/MaximalSquare221.java
+++ src/leetcode/algo/dp/matrix/MaximalSquare221.java
@@ -28,8 +28,8 @@ public class MaximalSquare221 {
         return maxSquare;
     }
 
-    // time
-    // space
+    // time O(n*m)
+    // space O(n*m)
     class DFS {
         int res = 0;
 
@@ -64,5 +64,47 @@ public class MaximalSquare221 {
             res = Math.max(res, visited[i][j]);
             return visited[i][j];
         }
+    }
+    //time O(n^m)
+    //space O(n)
+    class Simulation {
+        public int maximalSquare(char[][] matrix) {
+            int maxSide = 0;
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return maxSide;
+            }
+            int rows = matrix.length, columns = matrix[0].length;
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    if (matrix[i][j] == '1') {
+                        // 遇到一个 1 作为正方形的左上角
+                        maxSide = Math.max(maxSide, 1);
+                        // 计算可能的最大正方形边长
+                        int currentMaxSide = Math.min(rows - i, columns - j);
+                        for (int k = 1; k < currentMaxSide; k++) {
+                            // 判断新增的一行一列是否均为 1
+                            boolean flag = true;
+                            if (matrix[i + k][j + k] == '0') {
+                                break;
+                            }
+                            for (int m = 0; m < k; m++) {
+                                if (matrix[i + k][j + m] == '0' || matrix[i + m][j + k] == '0') {
+                                    flag = false;
+                                    break;
+                                }
+                            }
+                            if (flag) {
+                                maxSide = Math.max(maxSide, k + 1);
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            int maxSquare = maxSide * maxSide;
+            return maxSquare;
+        }
+
     }
 }
