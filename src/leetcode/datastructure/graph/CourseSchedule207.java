d42b2eabe6bda2e854cc2c32a0f3d4b1d235107b
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-12-27 01:00:42 +0100

    CourseSchedule 207

--- src/leetcode/datastructure/graph/CourseSchedule207.java
+++ src/leetcode/datastructure/graph/CourseSchedule207.java
@@ -48,4 +48,35 @@ public class CourseSchedule207 {
         }
         return learnCourses == courses.length;
     }
+
+    // time O(N+M)
+    // space O(N+M)
+    class DFS {
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            List<List<Integer>> adjacency = new ArrayList<>();
+            for (int i = 0; i < numCourses; i++)
+                adjacency.add(new ArrayList<>());
+            int[] flags = new int[numCourses];
+            for (int[] cp : prerequisites)
+                adjacency.get(cp[1]).add(cp[0]);
+            for (int i = 0; i < numCourses; i++)
+                if (!dfs(adjacency, flags, i))
+                    return false;
+            return true;
+        }
+
+        private boolean dfs(List<List<Integer>> adjacency, int[] flags, int i) {
+            if (flags[i] == 1)
+                return false;
+            if (flags[i] == -1)
+                return true;
+            flags[i] = 1;
+            for (Integer j : adjacency.get(i))
+                if (!dfs(adjacency, flags, j))
+                    return false;
+            flags[i] = -1;
+            return true;
+        }
+
+    }
 }
