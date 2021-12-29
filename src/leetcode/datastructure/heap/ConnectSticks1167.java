823f7f5b2717084e3615323a4a1bda83b3364354
underwindfall <14819756+underwindfall@users.noreply.github.com>
2021-12-29 21:52:51 +0100

    ConnectSticks 1167

--- src/leetcode/datastructure/heap/ConnectSticks1167.java
+++ src/leetcode/datastructure/heap/ConnectSticks1167.java
@@ -2,19 +2,19 @@ package leetcode.datastructure.heap;
 
 import java.util.PriorityQueue;
 
-// https://leetcode.com/problems/minimum-cost-to-connect-sticks/
+// https://leetcode-cn.com/problems/minimum-cost-to-connect-sticks/
 public class ConnectSticks1167 {
-    // time O(n)
-    // space O(n)
+    // time O(N * logN)
+    // space O(N)
     public int connectSticks(int[] sticks) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i : sticks) {
-            pq.add(i);
+            pq.offer(i);
         }
         int cost = 0;
         while (pq.size() != 1) {
             int min = pq.poll() + pq.poll();
-            pq.add(min);
+            pq.offer(min);
             cost += min;
         }
         return cost;
