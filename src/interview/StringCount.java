bcbe1038d15e9fe8ae1f9cc6724c4c45fdec464b
underwindfall <14819756+underwindfall@users.noreply.github.com>
2022-01-08 22:37:26 +0100

    String count

---
+++ src/interview/StringCount.java
@@ -0,0 +1,36 @@
+package interview;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringCount {
+    /**
+     * given a string, find the total number of repetitions. Repetition means if a
+     * word appear more than once, and repetition number is the times minus one.
+     * for example: The sun is the largest object in the solar system. It is the
+     * only star.
+     * "the" apprears 4 times, "is" appears 2 time so return
+     * valu‍‍‌‍‌‌‌‌‍‍‌‍‌‍‍‍‍‍‌‌e is 3+1 = 4
+     */
+
+    public static void main(String[] args) {
+        String input = "The sun is the largest object in the solar system. It is the only star";
+        System.out.println(findCount(input, "the"));
+    }
+
+    static int findCount(String input, String target) {
+        if (input.length() == 0)
+            return 0;
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : input.split(" ")) {
+            map.put(s.toLowerCase(), map.getOrDefault(s.toLowerCase(), 0) + 1);
+        }
+
+        if (map.containsKey(target.toLowerCase())) {
+            return map.get(target);
+        } else {
+            return -1;
+        }
+    }
+
+}
