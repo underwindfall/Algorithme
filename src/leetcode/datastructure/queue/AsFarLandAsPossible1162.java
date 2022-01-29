9c01b82247568ef3c17080d50f7d4a118bb5b61d
underwindfall <14819756+underwindfall@users.noreply.github.com>
2022-01-29 15:33:45 +0100

    leetcode 1162 AsFarLandAsPossible

--- src/leetcode/datastructure/queue/AsFarLandAsPossible1162.java
+++ src/leetcode/datastructure/queue/AsFarLandAsPossible1162.java
@@ -1,9 +1,12 @@
 package leetcode.datastructure.queue;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 //https://leetcode-cn.com/problems/as-far-from-land-as-possible/
+//和LC1765类似
 public class AsFarLandAsPossible1162 {
 
     // time O(M*N)
@@ -13,9 +16,9 @@ public class AsFarLandAsPossible1162 {
     // 2.guessing考虑做两次动态规划，第一次从左上到右下，第二次从右下到左上，记 f(x, y)f(x,y) 为 (x, y)(x,y)
     // 距离最近的陆地区域的曼哈顿距离，则我们可以推出这样的转移方程
     // 3. 左上
-    //      dp(i, j) = min(dp(i - 1, j), dp(i, j - 1)) + 1
-    //    右下
-    //      dp(i, j) = min(dp(i + 1, j), dp(i, j + 1)) + 1
+    // dp(i, j) = min(dp(i - 1, j), dp(i, j - 1)) + 1
+    // 右下
+    // dp(i, j) = min(dp(i + 1, j), dp(i, j + 1)) + 1
     class DP {
         public int maxDistance(int[][] grid) {
             final int INF = 1000000;
@@ -74,6 +77,10 @@ public class AsFarLandAsPossible1162 {
     }
 
     /**
+     * 虚拟源点
+     */
+
+    /**
      * 海洋到陆地的最大距离 <====> 陆地到附近海洋的最远距离
      * 因为我们只要先把所有的陆地都入队，然后从各个陆地同时开始一层一层的向海洋扩散，那么最后扩散到的海洋就是最远的海洋！
      * 并且这个海洋肯定是被离他最近的陆地给扩散到的！
@@ -130,6 +137,54 @@ public class AsFarLandAsPossible1162 {
 
         boolean isValid(int[][] matrix, int row, int col) {
             return row >= 0 && row < matrix.length && col >= 0 && col < matrix[0].length;
+        }
+    }
+
+    /**
+     * 从海洋出发
+     * // time O(n^4)
+     * //space O(n^2)
+     */
+    class BFSFromSea {
+        public int maxDistance(int[][] grid) {
+            int n = grid.length;
+            int ans = -1;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 0) {
+                        ans = Math.max(ans, bfs(i, j, grid));
+                    }
+                }
+            }
+            return ans;
+        }
+
+        int bfs(int row, int col, int[][] grid) {
+            int n = grid.length;
+            int[][] dirs = new int[][] { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 } };
+            Queue<int[]> q = new LinkedList<>();
+            Map<Integer, Integer> map = new HashMap<>();
+            q.offer(new int[] { row, col });
+            map.put(n * row + col, 0);
+            while (!q.isEmpty()) {
+                int[] poll = q.poll();
+                int dx = poll[0], dy = poll[1];
+                int step = map.get(dx * n + dy);
+                if (grid[dx][dy] == 1) {
+                    return step;
+                }
+                for (int[] dir : dirs) {
+                    int nx = dx + dir[0], ny = dy + dir[1];
+                    if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+                        continue;
+                    int key = nx * n + ny;
+                    if (map.containsKey(key))
+                        continue;
+                    q.offer(new int[] { nx, ny });
+                    map.put(key, step + 1);
+                }
+            }
+            return -1;
         }
     }
 }
