982159c04f549e8c5fa3f8e681f425215c9ca208
underwindfall <14819756+underwindfall@users.noreply.github.com>
2022-02-15 21:26:08 +0100

    LCOF09

---
+++ src/swordoffer/LCOF09.java
@@ -0,0 +1,37 @@
+package swordoffer;
+
+import java.util.Stack;
+
+// https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+public class LCOF09 {
+    // time O(n)
+    // space O(n)
+    class CQueue {
+        Stack<Integer> stack1;
+        Stack<Integer> stack2;
+
+        public CQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
+
+        public void appendTail(int value) {
+            stack1.push(value);
+        }
+
+        public int deleteHead() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+                if (stack2.isEmpty())
+                    return -1;
+                else
+                    return stack2.pop();
+            } else {
+                return stack2.pop();
+            }
+        }
+    }
+
+}
