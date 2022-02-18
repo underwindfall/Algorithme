4ea6799a8868e2ed7312ae0fc0a558f01097a909
underwindfall <14819756+underwindfall@users.noreply.github.com>
2022-02-18 22:11:01 +0100

    LCOF57

---
+++ src/swordoffer/LCOF57.java
@@ -0,0 +1,24 @@
+package swordoffer;
+
+//https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/
+public class LCOF57 {
+    // time O(n)
+    // space O(1)
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int s = nums[i] + nums[j];
+            if (s < target) {
+                i++;
+            } else if (s == target) {
+                res[0] = nums[i];
+                res[1] = nums[j];
+                break;
+            } else if (s > target) {
+                j--;
+            }
+        }
+        return res;
+    }
+}
