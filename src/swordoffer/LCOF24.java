18f29f150676af15059577f1816e5f111e8bc507
underwindfall <14819756+underwindfall@users.noreply.github.com>
2022-02-19 17:52:33 +0100

    LCOF24

---
+++ src/swordoffer/LCOF24.java
@@ -0,0 +1,40 @@
+package swordoffer;
+
+//https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+public class LCOF24 {
+    // time O(n)
+    // space O(1)
+    class Recursive {
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode next = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return next;
+        }
+    }
+
+    class DummyNode {
+        public ListNode reverseList(ListNode head) {
+            ListNode dummy = new ListNode(-1);
+            while (head.next != null) {
+                ListNode next = head.next;
+                head.next = dummy.next;
+                dummy.next = head;
+                head = next;
+            }
+            return dummy.next;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
