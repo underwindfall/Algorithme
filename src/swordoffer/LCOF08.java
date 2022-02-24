7febf8c788f0b029e5d49085f232f34ddd09ef66
underwindfall <14819756+underwindfall@users.noreply.github.com>
2022-02-24 17:06:28 +0100

    LCOF08

---
+++ src/swordoffer/LCOF08.java
@@ -0,0 +1,37 @@
+package swordoffer;
+
+// https://www.nowcoder.com/practice/9023a0c988684a53960365b889ceaf5e
+public class LCOF08 {
+    // time O(n)
+    // space O(1)
+    //left root right
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+        if (pNode.right != null) {
+            TreeLinkNode node = pNode.right;
+            while (node.left != null) {
+                node = node.left;
+            }
+            return node;
+        } else {
+            while (pNode.next != null) {
+                TreeLinkNode parent = pNode.next;
+                if (parent.left == pNode) {
+                    return parent;
+                }
+                pNode = pNode.next;
+            }
+        }
+        return null;
+    }
+
+    class TreeLinkNode {
+        int val;
+        TreeLinkNode left = null;
+        TreeLinkNode right = null;
+        TreeLinkNode next = null;
+
+        TreeLinkNode(int val) {
+            this.val = val;
+        }
+    }
+}
