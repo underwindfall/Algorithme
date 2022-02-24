40cac194ee5551f1ee5f5cc75d8ef5d9baa1e082
underwindfall <14819756+underwindfall@users.noreply.github.com>
2022-02-24 18:00:42 +0100

    LCOF26

---
+++ src/swordoffer/LCOF26.java
@@ -0,0 +1,28 @@
+package swordoffer;
+
+// https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof/
+public class LCOF26 {
+    // time O(n)
+    // space O(n)
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        return (A != null && B != null && dfs(A, B)) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    boolean dfs(TreeNode A, TreeNode B) {
+        if (B == null)
+            return true;
+        if (A != null && A.val != B.val)
+            return false;
+        return dfs(A.left, B.left) && dfs(A.right, B.right);
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
