f210a08a739fa3381d37c02f1fa6aa0fa4cbc5d1
underwindfall <14819756+underwindfall@users.noreply.github.com>
2022-02-24 17:06:28 +0100

    LCOF08

---
+++ src/swordoffer/LCOF08.java
@@ -0,0 +1,38 @@
+package swordoffer;
+
+// https://www.nowcoder.com/practice/9023a0c988684a53960365b889ceaf5e
+public class LCOF08 {
+    // time O(n)
+    // space O(1)
+    // left root right
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+        if (pNode.right != null) {
+            TreeLinkNode node = pNode.right;
+            while (node.left != null) {
+                node = node.left;
+            }
+            return node;
+        } else {
+            while (pNode.next != null) {
+                TreeLinkNode parent = pNode.next;
+                if (parent.left == pNode) {
+                    return parent;
+                }
+                pNode = pNode.next;
+            }
+        }
+        return null;
+    }
+
+    // 给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回 。注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
+    class TreeLinkNode {
+        int val;
+        TreeLinkNode left = null;
+        TreeLinkNode right = null;
+        TreeLinkNode next = null; // 指向父结点的指针
+
+        TreeLinkNode(int val) {
+            this.val = val;
+        }
+    }
+}
