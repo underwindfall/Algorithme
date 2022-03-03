9fc55316c8fddb7dc88965d37f7dc5ccbe617f65
underwindfall <14819756+underwindfall@users.noreply.github.com>
2022-03-03 15:36:43 +0100

    NumberOfArithmeticSlices413

--- src/leetcode/algo/dp/NumberOfArithmeticSlices413.java
+++ src/leetcode/algo/dp/NumberOfArithmeticSlices413.java
@@ -2,6 +2,47 @@ package leetcode.algo.dp;
 
 // https://leetcode-cn.com/problems/arithmetic-slices/
 public class NumberOfArithmeticSlices413 {
+    /**
+     * 思路：
+     * 
+     * 定义状态 dp[i] : 以 nums[ i ] 结尾的等差子数列的个数， 对 dp[ i ] ( 0 <= i < n ) 求和 就得到 nums 中
+     * 等差数列的总的个数；
+     * 
+     * 状态初始化 ： 由于题目要求的等差数列长度不能小于3 ，dp[ 0 ] = 0, dp [ 1 ] = 0;
+     * 
+     * 状态转移方程 ： 举栗子 nums = [1,2,3,4,5,7 ] , 
+     * dp[0] = 0, 
+     * dp[1] = 0, 
+     * 对 dp[2] , nums[2] - nums[1] == nums[1] - nums[0] , dp[2] = dp[1] + 1 = 0 + 1; 
+     * dp[2] 相对于 dp[1] 多了一个以 nums[2] 结尾的 [1,2,3]
+     * 对 dp[3] , nums[3] - nums[2] ==nums[2] - nums[1] ,
+     * dp[3] = dp[2] + 1 = 0 + 1; dp[3] 相对于 dp[2] 多了一个以 nums[] 结尾的 [2,3,4]; . . . 对
+     * dp[5] , nums[5] - nums[4] != nums[4] - nums[3] , dp[5] = 0;
+     * 
+     * dp[i] = nums[i] - nums[i-1] == nums[i-1] - nums[i-2] ? dp[i-1] + 1 : 0;
+     * 
+     * 因为 dp [i] 只和 dp[i-1] 有关， 所以使用一个变量记录状态转移就可以了
+     */
+    //time O(n)
+    //space O(n)
+    class DP {
+        public int numberOfArithmeticSlices(int[] A) {
+            int dp[] = new int[A.length];
+            dp[0] = 0; 
+            dp [1] = 0;
+            int count = 0;
+            for (int i = 2; i < A.length; i++) {
+                if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                    dp[i] = dp[i - 1] + 1;
+                    count += dp[i];
+                } else {
+                    continue;
+                }
+            }
+            return count;
+        }
+    }
+
     // the way to iteration
     // 第一层循环从0 - 数组倒数第二位
     // 第二层循环比较离第一层两个数字的距离 因为是等差数列最少两个
@@ -25,6 +66,9 @@ public class NumberOfArithmeticSlices413 {
         }
     }
 
+    //https://leetcode-cn.com/problems/arithmetic-slices/solution/fu-xue-ming-zhu-bao-li-shuang-zhi-zhen-d-fc1l/
+    //time O(n^2)
+    //space O(n)
     public class Recursive {
         int sum = 0;
 
@@ -43,24 +87,6 @@ public class NumberOfArithmeticSlices413 {
             } else
                 slices(A, i - 1);
             return ap;
-        }
-    }
-
-    // 和递归深思
-    // 递归的规律就是 dp[i] = dp[i - 1] + 1
-    class DP {
-        public int numberOfArithmeticSlices(int[] A) {
-            int dp[] = new int[A.length];
-            int count = 0;
-            for (int i = 2; i < A.length; i++) {
-                if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
-                    dp[i] = dp[i - 1] + 1;
-                    count += dp[i];
-                } else {
-                    continue;
-                }
-            }
-            return count;
         }
     }
 
