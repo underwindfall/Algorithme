aa5adc873185075ab67e3910bff8e3c4a3768699
underwindfall <14819756+underwindfall@users.noreply.github.com>
2022-03-22 21:50:35 +0100

    WinnerOfGames2038

---
+++ src/leetcode/algo/greedy/WinnerOfGames2038.java
@@ -0,0 +1,19 @@
+package leetcode.algo.greedy;
+
+// https://leetcode-cn.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/s
+public class WinnerOfGames2038 {
+    // time O(n)
+    // space O(1)
+    public boolean winnerOfGame(String colors) {
+        char[] cs = colors.toCharArray();
+        int n = cs.length;
+        int a = 0, b = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (cs[i] == 'A' && cs[i - 1] == 'A' && cs[i + 1] == 'A')
+                a++;
+            if (cs[i] == 'B' && cs[i - 1] == 'B' && cs[i + 1] == 'B')
+                b++;
+        }
+        return a > b;
+    }
+}
