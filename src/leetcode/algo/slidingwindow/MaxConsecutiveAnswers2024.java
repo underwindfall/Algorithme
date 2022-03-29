a73fd1277a344963b7fda6e4a53130211dd67d7b
underwindfall <14819756+underwindfall@users.noreply.github.com>
2022-03-29 16:22:35 +0200

    MaxConsecutiveAnswers2024

---
+++ src/leetcode/algo/slidingwindow/MaxConsecutiveAnswers2024.java
@@ -0,0 +1,24 @@
+package leetcode.algo.slidingwindow;
+
+// https://leetcode-cn.com/problems/maximize-the-confusion-of-an-exam/
+public class MaxConsecutiveAnswers2024 {
+    // time O(n*2)
+    // space O(1)
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        return Math.max(maxConsecutiveChar(answerKey, k, 'T'),
+                maxConsecutiveChar(answerKey, k, 'F'));
+    }
+
+    int maxConsecutiveChar(String answerKey, int k, char ch) {
+        int n = answerKey.length();
+        int ans = 0;
+        for (int left = 0, right = 0, sum = 0; right < n; right++) {
+            sum += answerKey.charAt(right) != ch ? 1 : 0;
+            while (sum > k) {
+                sum -= answerKey.charAt(left++) != ch ? 1 : 0;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+    }
+}
