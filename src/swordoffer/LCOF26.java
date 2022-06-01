75a90028f6ec2e89760ee9002d03b56ea682d7bd
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-06-02 00:29:07 +0800

    LCOF26

--- src/swordoffer/LCOF26.java
+++ src/swordoffer/LCOF26.java
@@ -5,13 +5,17 @@ public class LCOF26 {
     // time O(n)
     // space O(n)
     public boolean isSubStructure(TreeNode A, TreeNode B) {
-        return (A != null && B != null && dfs(A, B)) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+        return (A != null && B != null && dfs(A, B))
+                || A != null && isSubStructure(A.left, B)
+                || A != null && isSubStructure(A.right, B);
     }
 
     boolean dfs(TreeNode A, TreeNode B) {
         if (B == null)
             return true;
-        if (A == null && A.val != B.val)
+        if (A == null)
+            return false;
+        if (A.val != B.val)
             return false;
         return dfs(A.left, B.left) && dfs(A.right, B.right);
     }
