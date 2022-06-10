d0c934d76e47c384f8d39119462949390e8afcf9
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-06-10 16:16:03 +0800

    CountDiffPanlindromic730

--- src/leetcode/algo/dp/doublesequence/interval/CountDiffPanlindromic730.java
+++ src/leetcode/algo/dp/doublesequence/interval/CountDiffPanlindromic730.java
@@ -2,27 +2,32 @@ package leetcode.algo.dp.doublesequence.interval;
 
 //https://leetcode-cn.com/problems/count-different-palindromic-subsequences/
 public class CountDiffPanlindromic730 {
-    //这里的子问题的分析很巧妙
-    //S[i, j] count of different 回文数量
-    // dp[i, j] = 
-    //  if s[i] != s[j] :                                                   # count("abcd")
-    //      dp[i][j - 1] + dp[i + 1][j] - dp[i + 1][j - 1]                  # count("abc") + count("bcd") - count("bc")
-    //  if s[i] == s[j]
-    //      dp[i + 1][j - 1] * 2 + 2                                        s[i] not in s[i + 1: j - 1]    count("acbca")
-    //      dp[i + 1][j - 1] * 2 + 1                                        count(s[i + 1: j -1], s[i]) == 1       count("bcbcb")
-    //      dp[i + 1][j - 1] * 2 - dp[l + 1][r - 1]                         l, r is the first/last pos of s[i] in s[i + 1: j -1] count("bcab")                                 
+    // https://leetcode.cn/problems/count-different-palindromic-subsequences/solution/tong-ji-butong-by-jiang-hui-4-q5xf/
+    // 这里的子问题的分析很巧妙
+    // S[i, j] count of different 回文数量
+    // dp[i, j] =
+    // if s[i] != s[j] : # count("abcd")
+    // dp[i][j - 1] + dp[i + 1][j] - dp[i + 1][j - 1] # count("abc") + count("bcd")
+    // - count("bc")
+    // if s[i] == s[j]
+    // dp[i + 1][j - 1] * 2 + 2 s[i+1,j-1]中没有字符和s[i]相等 s[i] not in s[i + 1: j - 1]
+    // count("acbca")
+    // dp[i + 1][j - 1] * 2 + 1 s[i+1,j-1]中有一个字符和s[i]相等 count(s[i + 1: j -1], s[i])
+    // == 1 count("bcbcb")
+    // dp[i + 1][j - 1] * 2 - dp[l + 1][r - 1] s[i+1,j-1]中有两个及以上字符和s[i]相等 l, r is
+    // the first/last pos of s[i] in s[i + 1: j -1] count("a dabcbad a")
 
-     class MemoRecursive {
+    class MemoRecursive {
         int[][] m;
         private static final int kMod = 1000000007;
-        
+
         public int countPalindromicSubsequences(String S) {
             int n = S.length();
             m = new int[n][n];
             return count(S.toCharArray(), 0, n - 1);
         }
 
-        int count(char[]s , int i, int j) {
+        int count(char[] s, int i, int j) {
             if (i > j)
                 return 0;
             if (i == j)
@@ -38,11 +43,11 @@ public class CountDiffPanlindromic730 {
                     l++;
                 while (l <= r && s[r] != s[i])
                     r--;
-                if (l > r) {//s[i] not in s[i + 1: j - 1]    
+                if (l > r) {// s[i] not in s[i + 1: j - 1]
                     ans += count(s, i + 1, j - 1) * 2 + 2;
-                } else if ( l == r) { //count(s[i + 1: j -1], s[i]) == 1
+                } else if (l == r) { // count(s[i + 1: j -1], s[i]) == 1
                     ans += count(s, i + 1, j - 1) * 2 + 1;
-                } else {//l, r is the first/last pos of s[i] in s[i + 1: j -1]
+                } else {// l, r is the first/last pos of s[i] in s[i + 1: j -1]
                     ans += count(s, i + 1, j - 1) * 2 - count(s, l + 1, r - 1);
                 }
             } else {
@@ -53,36 +58,54 @@ public class CountDiffPanlindromic730 {
         }
     }
 
+    // time O(n^2)
+    // space O(n^2)
     class DP {
-        
-        private static final int kMod = 1000000007;
 
-        public int countPalindromicSubsequences(String S) {
-            int n = S.length();
-            long dp[][] = new long[n][n];
+        public int countPalindromicSubsequences(String s) {
+            int mod = (int) (1e9) + 7;
+            int n = s.length();
+            int[][] dp = new int[n][n];
+            // 一个单字符是一个回文子序列
             for (int i = 0; i < n; i++) {
-                //只有一个字符串的解
                 dp[i][i] = 1;
             }
-
-            for (int len = 1; len <= n; len++) {
-                for (int i = 0; i < n - len; i++) {
-                    int j = i + len;
-                    if (S.charAt(i) == S.charAt(j)) {
-                        int l = i + 1;
-                        int r = j - 1;
-                        while (l <= r && S.charAt(l) != S.charAt(i)) ++l;
-                        while (l <= r && S.charAt(r) != S.charAt(i)) --r;                    
-                        if (l == r) dp[i][j] = dp[i + 1][j - 1] * 2 + 1;
-                        else if (l > r) dp[i][j] = dp[i + 1][j - 1] * 2 + 2;
-                        else dp[i][j] = dp[i + 1][j - 1] * 2 - dp[l + 1][r - 1];
+            // 从长度为2的子串开始计算
+            for (int len = 2; len <= n; len++) {
+                // 挨个计算长度为len的子串的回文子序列个数
+                for (int i = 0; i + len <= n; i++) {
+                    int j = i + len - 1;
+                    // 情况(1) 相等
+                    if (s.charAt(i) == s.charAt(j)) {
+                        int left = i + 1;
+                        int right = j - 1;
+                        // 找到第一个和s[i]相同的字符
+                        while (left <= right && s.charAt(left) != s.charAt(i)) {
+                            left++;
+                        }
+                        // 找到第一个和s[j]相同的字符
+                        while (left <= right && s.charAt(right) != s.charAt(j)) {
+                            right--;
+                        }
+                        if (left > right) {
+                            // 情况① 没有重复字符
+                            dp[i][j] = 2 * dp[i + 1][j - 1] + 2;
+                        } else if (left == right) {
+                            // 情况② 出现一个重复字符
+                            dp[i][j] = 2 * dp[i + 1][j - 1] + 1;
+                        } else {
+                            // 情况③ 有两个及两个以上
+                            dp[i][j] = 2 * dp[i + 1][j - 1] - dp[left + 1][right - 1];
+                        }
                     } else {
-                        dp[i][j] = dp[i][j - 1] + dp[i + 1][j] - dp[i + 1][j - 1]; 
+                        // 情况(2) 不相等
+                        dp[i][j] = dp[i][j - 1] + dp[i + 1][j] - dp[i + 1][j - 1];
                     }
-                    dp[i][j] = (long)((dp[i][j] + kMod) % kMod);
+                    // 处理超范围结果
+                    dp[i][j] = (dp[i][j] >= 0) ? dp[i][j] % mod : dp[i][j] + mod;
                 }
             }
-            return (int)dp[0][n - 1];
+            return dp[0][n - 1];
         }
     }
 }
