392e42099e51ec07bb98f6b5d1860f6d46e9e021
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-08-12 22:06:15 +0800

    SortedSquares977

---
+++ src/leetcode/algo/doubleindex/SortedSquares977.java
@@ -0,0 +1,22 @@
+package leetcode.algo.doubleindex;
+
+// https://leetcode.cn/problems/squares-of-a-sorted-array/
+public class SortedSquares977 {
+    // time O(n)
+    // space O(1)
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0, j = n - 1, pos = n - 1; i <= j;) {
+            if (nums[i] * nums[i] > nums[j] * nums[j]) {
+                ans[pos] = nums[i] * nums[i];
+                i++;
+            } else {
+                ans[pos] = nums[j] * nums[j];
+                j--;
+            }
+            pos--;
+        }
+        return ans;
+    }
+}
