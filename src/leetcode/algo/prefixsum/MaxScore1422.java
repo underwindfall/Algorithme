613b550a3c7571466bd67e1a6b5ec39f7140a067
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-08-15 15:00:23 +0800

    MaxScore1422

---
+++ src/leetcode/algo/prefixsum/MaxScore1422.java
@@ -0,0 +1,30 @@
+package leetcode.algo.prefixsum;
+
+// https://leetcode.cn/problems/maximum-score-after-splitting-a-string/
+public class MaxScore1422 {
+    // time O(N)
+    // space O(1)
+    public int maxScore(String s) {
+        int score = 0;
+        int n = s.length();
+        if (s.charAt(0) == '0') {
+            score++;
+        }
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                score++;
+            }
+        }
+        int ans = score;
+        for (int i = 1; i < n - 1; i++) {
+            if (s.charAt(i) == '0') {
+                score++;
+            } else {
+                score--;
+            }
+            ans = Math.max(ans, score);
+        }
+        return ans;
+
+    }
+}
