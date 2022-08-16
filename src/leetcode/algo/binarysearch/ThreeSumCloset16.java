8dac5858f2b3e1e001a946bf6e18ae4ffc5c9d02
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-08-16 15:49:32 +0800

    ThreeSumCloset16

---
+++ src/leetcode/algo/binarysearch/ThreeSumCloset16.java
@@ -0,0 +1,28 @@
+package leetcode.algo.binarysearch;
+
+import java.util.Arrays;
+
+// https://leetcode.cn/problems/3sum-closest/
+public class ThreeSumCloset16 {
+    // time O(n * logN)
+    // space O(N)
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length; i++) {
+            int start = i + 1, end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[start] + nums[end] + nums[i];
+                if (Math.abs(target - sum) < Math.abs(target - ans))
+                    ans = sum;
+                if (sum > target)
+                    end--;
+                else if (sum < target)
+                    start++;
+                else
+                    return ans;
+            }
+        }
+        return ans;
+    }
+}
