3d706c64c56edb75aae54279ad64d4ad724a0700
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-08-23 14:33:21 +0800

    SpiralMatrix

---
+++ src/leetcode/datastructure/array/SpiralMatrix54.java
@@ -0,0 +1,39 @@
+
+package leetcode.datastructure.array;
+
+// https://leetcode.cn/problems/spiral-matrix/
+class SpiralMatrix54 {
+    public int[][] generateMatrix(int n) {
+        // time O(N^2)
+        // space O(N^2)
+        int[][] ans = new int[n][n];
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+        int count = 1;
+        while (count <= n * n) {
+            for (int i = left; i <= right && count <= n * n; i++) {
+                ans[top][i] = count;
+                count++;
+            }
+            top++;
+
+            for (int i = top; i <= bottom && count <= n * n; i++) {
+                ans[i][right] = count;
+                count++;
+            }
+            right--;
+
+            for (int i = right; i >= left && count <= n * n; i--) {
+                ans[bottom][i] = count;
+                count++;
+            }
+            bottom--;
+
+            for (int i = bottom; i >= top && count <= n * n; i--) {
+                ans[i][left] = count;
+                count++;
+            }
+            left++;
+        }
+        return ans;
+    }
+}
