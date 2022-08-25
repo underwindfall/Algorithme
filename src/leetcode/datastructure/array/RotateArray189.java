d859b7f7e3e2059afb79264c037a650b1a213397
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-08-25 21:19:36 +0800

    RotateArray189

---
+++ src/leetcode/datastructure/array/RotateArray189.java
@@ -0,0 +1,23 @@
+package leetcode.datastructure.array;
+
+// https://leetcode.cn/problems/rotate-array/
+public class RotateArray189 {
+    // time O(n)
+    // space O(1)
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+}
