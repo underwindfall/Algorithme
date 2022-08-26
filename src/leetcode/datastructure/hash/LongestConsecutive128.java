dda597f20cc0e418e1257adfeee25af1895bbd88
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-08-26 15:06:57 +0800

    LongestConsecutive128

---
+++ src/leetcode/datastructure/hash/LongestConsecutive128.java
@@ -0,0 +1,35 @@
+package leetcode.datastructure.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// https://leetcode.cn/problems/longest-consecutive-sequence/
+public class LongestConsecutive128 {
+    // time O(n)
+    // space O(n)
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> numsSet = new HashSet<>();
+        for (Integer num : nums) {
+            numsSet.add(num);
+        }
+        int longest = 0;
+        for (Integer num : nums) {
+            if (numsSet.remove(num)) {
+                // 向当前元素的左边搜索,eg: 当前为100, 搜索：99，98，97,...
+                int currentLongest = 1;
+                int current = num;
+                while (numsSet.remove(current - 1))
+                    current--;
+                currentLongest += (num - current);
+                // 向当前元素的右边搜索,eg: 当前为100, 搜索：101，102，103,...
+                current = num;
+                while (numsSet.remove(current + 1))
+                    current++;
+                currentLongest += (current - num);
+                // 搜索完后更新longest.
+                longest = Math.max(longest, currentLongest);
+            }
+        }
+        return longest;
+    }
+}
