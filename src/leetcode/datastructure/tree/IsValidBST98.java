a2f4ad97c8baa3a6e22df7a6eafc13fe834bee67
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-08-30 23:47:19 +0800

    IsValidBST98

--- src/leetcode/datastructure/tree/IsValidBST98.java
+++ src/leetcode/datastructure/tree/IsValidBST98.java
@@ -1,5 +1,8 @@
 package leetcode.datastructure.tree;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 // 判断一个tree是不是BST
 // 关键在于是否满足左子树的最大值<root.val<右子树的最小值
 //https://leetcode-cn.com/problems/validate-binary-search-tree
@@ -14,8 +17,8 @@ public class IsValidBST98 {
         }
     }
 
-    //time O(N)
-    //espace O(N)
+    // time O(N)
+    // espace O(N)
     // 如何判断一个tree 是不是BST
     public boolean isValidBST(TreeNode root) {
         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
@@ -29,6 +32,27 @@ public class IsValidBST98 {
             return false;
         }
         return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+
+    // time o(n)
+    //space O(n )
+    class Iterative {
+        public boolean isValidBST(TreeNode root) {
+            Deque<TreeNode> stk = new ArrayDeque<>();
+            TreeNode prev = null;
+            while (root != null || !stk.isEmpty()) {
+                while (root != null) {
+                    stk.push(root);
+                    root = root.left;
+                }
+                root = stk.pop();
+                if (prev != null && prev.val >= root.val)
+                    return false;
+                prev = root;
+                root = root.right;
+            }
+            return true;
+        }
     }
 
     // time O(N)
