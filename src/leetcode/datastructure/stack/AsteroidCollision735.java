ca409d7cbcd931bf81720222c2202481438997ec
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-09-27 15:20:13 +0800

    AsteroidCollision735

--- src/leetcode/datastructure/stack/AsteroidCollision735.java
+++ src/leetcode/datastructure/stack/AsteroidCollision735.java
@@ -1,16 +1,44 @@
 package leetcode.datastructure.stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 // https://leetcode-cn.com/problems/asteroid-collision/
 public class AsteroidCollision735 {
     // time O(n)
     // space O(n)
+    class MonoticDeque {
+        public int[] asteroidCollision(int[] asteroids) {
+            Deque<Integer> stack = new ArrayDeque<>();
+            for (int asteroid : asteroids) {
+                boolean isAlive = true;
+                while (isAlive && asteroid < 0 && !stack.isEmpty() && stack.peek() > 0) {
+                    isAlive = stack.peek() < -asteroid; // asteroid 是否存在
+                    if (stack.peek() <= -asteroid) {// 栈顶行星爆炸
+                        stack.pop();
+                    }
+                }
+                if (isAlive) {
+                    stack.push(asteroid);
+                }
+            }
+            int[] res = new int[stack.size()];
+            for (int i = res.length - 1; i >= 0; i--) {
+                res[i] = stack.pop();
+            }
+            return res;
+
+        }
+    }
+
+    // time O(n)
+    // space O(n)
     /**
-     *  入栈
-     *  1. 栈是空的
-     *  2. 栈顶元素是负数（下一个为负数一起向左， 下一个为正数则分向两边）
-     *  3. 当前元素为正数（栈顶为正一起向右， 栈顶为负分向两边）
+     * 入栈
+     * 1. 栈是空的
+     * 2. 栈顶元素是负数（下一个为负数一起向左， 下一个为正数则分向两边）
+     * 3. 当前元素为正数（栈顶为正一起向右， 栈顶为负分向两边）
      * 
      * 
      * ====== 碰撞
@@ -24,9 +52,8 @@ public class AsteroidCollision735 {
         while (p < asteroids.length) {
             if (stack.empty() || stack.peek() < 0 || asteroids[p] > 0) {
                 stack.push(asteroids[p]);
-            }
-            else if (stack.peek() <= -asteroids[p]) {
-                //栈顶的恒星爆炸
+            } else if (stack.peek() <= -asteroids[p]) {
+                // 栈顶的恒星爆炸
                 if (stack.pop() < -asteroids[p]) {
                     continue;
                 }
@@ -35,7 +62,7 @@ public class AsteroidCollision735 {
         }
 
         int[] res = new int[stack.size()];
-        for (int i = res.length - 1; i>=0; i--) {
+        for (int i = res.length - 1; i >= 0; i--) {
             res[i] = stack.pop();
         }
         return res;
